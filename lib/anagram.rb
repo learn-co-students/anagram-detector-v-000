8260fec761187fa44d14155d90599c82a6dcb1d4
jakebrady5 <jakebrady5@gmail.com>
2015-11-15 00:30:24 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,39 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :keyword, :anagrams
+
+  def initialize(word)
+    @keyword = word
+    @anagrams = []
+  end
+
+  def match(possibilities)
+    ana_finder = self.split_sort_join_array(possibilities)
+    word = split_sort_join_string(@keyword)
+
+    ana_finder.each_with_index{|a,i| 
+      if a == word
+        @anagrams << possibilities[i]
+      end
+      }
+
+    @anagrams
+  end
+
+  def split_sort_join_string(word)
+    sorted_word = word.split(//)
+    sorted_word = sorted_word.sort
+    sorted_word = sorted_word.join
+    sorted_word
+  end
+
+  def split_sort_join_array(possibilities)
+    possibilities = possibilities.collect{|a| a.split(//)}
+    possibilities = possibilities.collect{|a| a.sort}
+    possibilities = possibilities.collect{|a| a.join}
+    possibilities
+  end
+
+end
