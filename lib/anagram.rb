8dc7b182683dc6239f19de3ea4b231b5be701648
onyoo <roberto9m9@gmail.com>
2015-11-16 18:35:40 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :find_word
+
+  def initialize(new_target)
+    @find_word=(new_target)
+  end
+
+  def match(string)
+    ans = []
+    word = self.find_word.split(//).sort
+    string = string.map{|w| w = w.split(//)}
+    string.each do |w|
+      if w.sort == word
+        ans << w.join
+      end
+    end
+    ans
+  end
+end
+
