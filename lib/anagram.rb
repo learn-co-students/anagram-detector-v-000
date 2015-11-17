f04130597fe90d1ec02d1a7a1c7f5ac2844b64fb
Louiefigz <figueroa.aaron@gmail.com>
2015-11-17 16:26:11 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word 
+end 
+
+def match(array)
+  matched_array = []
+  # ["e", "h", "l" "l", "o"]
+  sorted_word = @word.split("").sort 
+ 
+  array.each do |word|
+    matched_array << word if word.split("").sort == sorted_word
+  end
+  matched_array
+end 
+
+
+end 
+
+# word = Anagram.new("hello")
+
+# word.match(array)
+
