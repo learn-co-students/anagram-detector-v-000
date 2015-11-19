0b15ccd52eb3c57cc2e6cc04b98ab2fd63c44888
Justin Thomas <jthomas0209@gmail.com>
2015-11-19 13:34:48 -0500

    Defined methods/variables in anagram.rb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  attr_accessor :word
+
+  def match(array)
+    output_array = []
+    array.each do |element|
+      output_array << element if word.split("").sort == element.split("").sort
+    end
+    output_array
+  end
+end
