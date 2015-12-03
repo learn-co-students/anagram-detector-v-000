fe1ed574cb91933a4ab974b2778ff9b78af881bf
surla <josephsurla@gmail.com>
2015-12-02 17:54:42 -0800

    create Anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = []
+    element = word.split("").sort
+    array.each do |words|
+      word_array << words if words.split("").sort == element
+    end
+    word_array
+  end
+end
