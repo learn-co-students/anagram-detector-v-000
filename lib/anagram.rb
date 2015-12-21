d9dd5e73432192b63a9d60cfbdd5b6cf7a0e6142
codeesh <vineshpersaud@gmail.com>
2015-12-21 10:25:57 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+@@ana = []
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(match)
+    @@ana.clear
+    sentence = (match)
+        sentence.each do |mixed|
+          x = mixed.split('') 
+          y = @word.split('')
+           if x.sort == y.sort
+              @@ana << mixed
+            end
+        end
+        @@ana   
+    end
+end
