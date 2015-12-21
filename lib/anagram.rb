0cffe3361077e54749c97721edb4af0e012070fb
kylegraydev <kylegraydev@gmail.com>
2015-12-20 23:58:05 -0800

    finished

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+#our code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(word_array)
+    answer = []
+    word_array.each do |word|
+      # binding.pry
+      if word.chars.sort.join == @word.chars.sort.join
+        answer << word
+      end
+    end
+    answer
+  end
+
+end
