03d1f7ac2f5bcb79310a6fbf8df7af1ecacb8595
Tsundu <osmentdan@gmail.com>
2015-12-23 14:20:53 -0500

    made anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word 
+
+  def initialize(word)
+    @word = word 
+
+  end 
+
+  def match(arr)
+    @ret = []
+  arr.each do |possible|
+    
+    if possible.split("").sort == @word.split("").sort 
+        @ret << possible
+      ### expect ba to equal (["ab"])
+    end 
+       
+  end 
+    @ret 
+  end 
+
+
+
+end 
