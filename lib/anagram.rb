ba24f73438bd981f629bd7a03dcacb0b69e847d7
eric-an <erican@me.com>
2015-12-26 00:13:39 -0800

    define an anagram finder method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    anagram_match = []
+
+    word_array.each do |letters|
+      if word.split("").sort == letters.split("").sort
+        anagram_match << letters
+      end
+    end
+    anagram_match
+  end
+end
