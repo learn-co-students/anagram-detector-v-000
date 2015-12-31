484d26552ef40329d05ae79827d704b258e983af
davdkm <davd.km@gmail.com>
2015-12-30 19:24:47 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    w_split = @word.split("").sort
+    match = []
+    array.each do |words|
+      element = words.split("").sort
+      match << words if w_split == element
+    end
+    match
+  end
+end
