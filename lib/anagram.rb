0cc22131e5238a094512ded9db8a4dd7ed566d2b
John Anthony <askinganthony@gmail.com>
2016-01-02 07:06:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def fits(test_word)
+    word_chars = @word.split(//)
+    match_chars = test_word.split(//)
+    tally=[]
+    match_chars.each do |match_char|
+      index = word_chars.find_index(match_char)
+      if index == nil
+        tally << false
+      else
+        word_chars.delete_at(index)
+        tally << true
+      end
+    end
+    !tally.include?(false)
+  end
+
+  def match(match_candidates_arr)
+    matches = []
+    match_candidates_arr.each {|candidate| matches << candidate if self.fits(candidate)}
+    matches
+  end
+end
