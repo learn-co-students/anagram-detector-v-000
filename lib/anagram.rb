0315fb3a0fc58611219c23716cba727ae555951a
tttra <tothutra@gmail.com>
2016-01-08 00:32:47 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    match = array.map do |x|
+      if x.split("").sort == @word.split("").sort
+        x
+      else
+        nil
+      end
+    end
+    match.compact
+  end
+
+end
+
