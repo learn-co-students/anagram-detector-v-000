5d87f64e888f3eeb20f89197d300581391636ee1
Brad1974 <danieltbrad@gmail.com>
2016-01-12 20:22:20 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select {|x| x.chars.sort.join == word.chars.sort.join}
+  end
+
+end
+
+
+
+# # Your code goes here!
+
+# array = ["enlists", "googxle", "inlets", "banana"]
+
+# array.collect {|x| x.chars.sort.join == word.chars.sort.join}
+
+
+# sorted_array = array.collect{|x| x.chars.sort.join}
+# sorted_array.include?(word.chars.sort.join)
+
+
+
+# array.select {|x| x.chars.sort.join == word.chars.sort.join}
+
+
+
