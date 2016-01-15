609d4e521bfd0ef1717c1767574d397817243310
fiveinfinity <jlachance1@gmail.com>
2016-01-14 19:01:27 -0600

    add match method to check if anagram is present in array

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+  solution = []
+
+    array.each do |i|
+      if i.split(//).sort == @word.split(//).sort
+        solution << i
+      end
+    end
+    solution
+  end
+end
