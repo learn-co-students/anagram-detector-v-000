bed470c607dce012116266a1d9d304afbe111262
simque <simon.duncanson@gmail.com>
2016-01-18 11:12:25 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,3 +17,24 @@ class Anagram
   the_word
 end
 end
+
+# RE-DID LAB WITHOUT LOOKING AT PREVIOUS SOLUTION
+# join method not necessary below
+# ternary operator too lengthy, practiced my use of next method
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    array.each do |string|
+      self.word.split("").sort.join == string.split("").sort.join ? matches << string : next
+    end
+    matches
+  end
+
+end
