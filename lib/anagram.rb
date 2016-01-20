8c51440de0ebe14cec79ff91b1019ddaac29365d
drywaterr24 <angelolea24@gmail.com>
2016-01-20 18:06:29 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words = words.collect do |a|
+      split_word = a.split("")
+      split_word.sort!
+      if split_word == @word.split("").sort
+        a
+      end
+    end
+    words.delete_if{|x| x == nil}
+  end
+end
