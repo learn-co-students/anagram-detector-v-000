f55a3d8d58a6cb5a9c0b3e110f8958a7772b6019
thegands <thegands@gmail.com>
2016-01-19 18:44:17 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    sorted_word = @word.chars.sort
+    array.find_all { |word| word.chars.sort == sorted_word }
+  end
+
+end
