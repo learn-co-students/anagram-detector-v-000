1c4d7a97ff6dffe448c05031f306f4e3eef09cbc
thegands <thegands@gmail.com>
2016-01-19 18:50:40 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,11 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-    @word = word
+    self.word = word
   end
 
   def match(array)
-    sorted_word = @word.chars.sort
+    sorted_word = self.word.chars.sort
     array.find_all { |word| word.chars.sort == sorted_word }
   end
 
