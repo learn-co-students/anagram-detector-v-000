361cbfe6cf2512240072358f662113252538240a
mystycs <mystycs@gmail.com>
2016-01-22 16:02:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    output = []
+    words.each do |x|
+      if @anagram.split("").sort == x.split("").sort
+        output << x
+      end
+    end
+    output
+  end
+
+
+end
