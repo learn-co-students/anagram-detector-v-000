293bf65c641fabda15ef1aa8d7e7eaf63380c09f
glee38 <glee3388@gmail.com>
2016-01-26 19:02:28 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    #.chars converts a word into an array of its characters.
+    #for example: "hello".chars => ["h", "e", "l", "l", "o"]
+    
+    words.select{|word| word.chars.sort == @anagram.chars.sort}
+  end
+end
