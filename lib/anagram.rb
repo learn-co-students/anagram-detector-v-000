db20c4e67dbeff19113af85ce80da997f18df0d2
glee38 <glee3388@gmail.com>
2016-01-26 19:06:24 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   def match(words)
     #.chars converts a word into an array of its characters.
     #for example: "hello".chars => ["h", "e", "l", "l", "o"]
-    
+    #the code below will select the full WORD that matches the block that is run through .select, not the separated character array that matches (code in the curly braces), since that is a second block iteration and the select is called on the array of the full words
     words.select{|word| word.chars.sort == @anagram.chars.sort}
   end
 end
