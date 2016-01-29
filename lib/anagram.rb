c6efe7f98351fcf55e73fa41bf4ed09c8728e288
Rafael A. Mejia <rafael@dataminded.net>
2016-01-29 03:33:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :seed
+
+  def initialize(seed)
+    @seed = seed
+  end
+
+  def match(terms)
+    list = terms.collect do |term|
+      if term.split("").sort == @seed.split("").sort
+        term
+      end
+    end
+    list.select {|term| term != nil}
+  end
+end
