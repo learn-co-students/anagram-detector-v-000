1048f0fa224446b0cb9dc7bfefee262d820c0657
Richard-Frost <frost.rchrd@gmail.com>
2016-01-29 19:10:22 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :words
+
+def initialize(words)
+  @words = words
+end
+
+def match(array)
+  new = []
+    array.each do |x| 
+    y = x.split("").sort
+    z = @words.split("").sort
+  
+  if y == z
+    new << x.to_s
+    
+  
+      end
+    end
+    new
+  end
+
+end
+
+#angrm_array = %W(#{words})
+ # angrm_array.map do |x|
+ #  y = x.split("")
