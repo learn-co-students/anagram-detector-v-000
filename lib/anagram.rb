aab79d07d41df4f5dff570cd6039cf44149e0821
zachnewburgh <zach.newburgh@gmail.com>
2016-02-01 09:16:26 -0500

    anagram detector method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.collect do |w|
+      if w.split("").sort == word.split("").sort
+        w
+      else
+        []
+      end
+    end.uniq.flatten
+  end
+
+end
