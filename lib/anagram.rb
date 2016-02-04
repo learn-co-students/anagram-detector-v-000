31ea0b302d9611b02f9e02a09ca88427d71d0694
evanscloud <dijkstrascloud@gmail.com>
2016-02-04 13:16:39 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :string
+
+  def initialize(word)
+    @string = word
+  end
+
+  def match(array)
+    array.select do |match_str|
+      match_str.split("").sort == @string.split("").sort
+    end
+  end
+end
