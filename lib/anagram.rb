24119e558218d579b23bae1c38575a0f4c793271
reifnotreef <bridgerrhammond@gmail.com>
2016-02-05 23:58:57 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(words)
+    words.find_all do |w| # cycle through all the words, use find_all to return a new array #
+      w.split("").sort == @name.split("").sort # split up the letters and sort them, compare this to the original input(which is also split and sorted) #
+    end
+  end
+
+end
