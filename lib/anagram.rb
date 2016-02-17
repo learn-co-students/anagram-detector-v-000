c96a929a489cbcc80b4aac7765e72698e1413938
Ann Lee <aplee29@users.noreply.github.com>
2016-02-17 04:26:44 -0500

    define Anagram class that returns an array of anagram matches

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    anagram_arr = []
+
+    word_array.each do |compare_word|
+      if compare_word.split("").sort == word.split("").sort
+        anagram_arr << compare_word
+      end
+    end
+    anagram_arr
+  end
+end
