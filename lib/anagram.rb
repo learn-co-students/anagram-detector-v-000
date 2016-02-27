274008dd6bf9e55bc7e1159e55d864f877f39e01
CodeByLine <yleventhal@ymail.com>
2016-02-27 22:54:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word, :word_list
+  matchedlist = []
+  orderedword = []
+
+  def initialize(word)
+    @word = word
+    @word_list = word_list
+    word = self.new
+    orderedword << word.split.sort
+  end
+
+  def self.match(word_list)
+    words = []
+    @word_list = word_list
+    words << word_list.split(" ").map(&:strip)
+    words.each.split do |matched|
+      if matched.sorted == orderedword
+        matchedlist << orderedword
+      end
+    return matchlist
+
+  end
+
+
+
+end
