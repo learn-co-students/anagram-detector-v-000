ccfdb70cb5482653186e706f942e911017527d3c
CodeByLine <yleventhal@ymail.com>
2016-02-28 06:31:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,26 +1,33 @@
 class Anagram
   attr_accessor :word, :word_list
   matchedlist = []
-  orderedword = []
+  wordsplit = []
 
   def initialize(word)
     @word = word
     @word_list = word_list
-    word = self.new
-    orderedword << word.split.sort
+    #word = self.create
+    #wordsplit << word.split.sort
   end
-
   def self.match(word_list)
-    words = []
-    @word_list = word_list
-    words << word_list.split(" ").map(&:strip)
-    words.each.split do |matched|
-      if matched.sorted == orderedword
-        matchedlist << orderedword
-      end
-    return matchlist
-
+    word_list.split(" ") do |words|
+      matchedlist << words.scan[/\w/]
+    end
   end
+
+
+#   def self.match(word_list)
+#    # words = []
+#     @word_list = word_list
+#     words << word_list.split(" ").map(&:strip)
+#     words.each.split do |matched|
+#       if matched.sorted == orderedword
+#         matchedlist << orderedword
+#       end
+#     return matchlist
+#     end
+
+#   end
 
 
 
