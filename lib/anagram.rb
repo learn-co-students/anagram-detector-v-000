dee7df8a190f166ddb5fce330bd8da5b17dce772
agdavid <antonio.g.david@gmail.com>
2016-02-29 17:21:29 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,11 +9,9 @@ class Anagram
 
   def match(possible_anagrams)
     original_split_sorted_word = @word.split(//).sort
-    possible_anagrams_array = []
-    possible_anagrams.each do |word|
-      possible_anagrams_array << word if word.split(//).sort == original_split_sorted_word
+    possible_anagrams.select do |word|
+      word.split(//).sort == original_split_sorted_word
     end
-    possible_anagrams_array
   end
 
 end
