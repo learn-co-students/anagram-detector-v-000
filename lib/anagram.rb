e415f96fb25a03f66559ddd376266c17ff3c0557
jsbenning <jbenninghoff1970@yahoo.com>
2016-02-29 21:15:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :testr
+  attr_accessor :bunch
+  
+  
+  def initialize(testr)
+    @testr = testr
+  end
+  
+  def match(bunch)
+    bunch.each do |word|
+      if word.to_a.sort == testr.to_a.sort
+        return word
+        else
+        next
+      end
+    end
+  end
+    
+
+end
