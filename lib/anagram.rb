a86cbd43ce066261d6343bbce12d0f90b5bc1b41
jbok4 <jbok4@aol.com>
2016-03-02 10:53:53 -0500

    cleaned up

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,11 +8,6 @@ class Anagram
   end
 
   def match(words) 
-    #want to sort the characters in each word alphabetically, then
-    #iterate over each word(array value) to see if it matches the given word
-    #if it does, push it into the new array.
-    #question is - what is the name of the array they are giving me 
-    #that i am checking over? how do i call it?
     array = []
     words.each do |x|
     if x.chars.sort == word.chars.sort
