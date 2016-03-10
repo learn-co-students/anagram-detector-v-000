8e36b6fc8dd50063b02b8fbb7656a8542f2ddbbf
bradleyoashburn <bradleyoashburn@gmail.com>
2016-03-09 15:19:01 -1000

    All tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.select {|anagram| 
+      anagram.split('').sort == @word.split('').sort
+    }
+  end
+
+end
+
