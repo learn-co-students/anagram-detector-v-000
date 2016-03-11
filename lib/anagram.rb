5fcae4f5f3ee973ed347939f86a36931cfc70680
aback1227 <aback1227@gmail.com>
2016-03-11 23:38:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,6 +19,8 @@ class Anagram
 #   end
 
 #more simplified version of above
+#won't have to make a new array like above bc with .select since 
+#it returns an array where block returns true
   def match(array)
     array.select {|jumbled| jumbled.split("").sort == @word.split("").sort}
   end
