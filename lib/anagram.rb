3b899c738d7f5ee175083e5f1b71d7e46e087378
rafeeq-123 <rafeeq-123@hotmail.com>
2016-03-14 18:54:27 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
-# Your code goes here!
+
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+def initialize(word)
+@word=word
+
+
+end
+
+def match(word)
+
+     word.select {|word|  word.chars.sort== @word.chars.sort}
+      
+    
+      
+  end
+ 
+end 
+
+    
+  
+
+
+
+
+
+
