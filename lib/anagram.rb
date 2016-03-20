900d1bf4b9e25cd59015f81f8d155dbebefc2161
McN33ly <rmcneely23@gmail.com>
2016-03-20 16:03:44 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(array)
+    array.keep_if{|word| word.split("").sort == @anagram.split("").sort}
+    
+  end
+#binding.pry
+
+end
