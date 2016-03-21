44e80af71066f91e25fc2f873be9c5c145986f45
PeteAJ <peter.jacobson@gmail.com>
2016-03-21 15:23:25 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :name, :artist_name
+
+    def initialize(word)
+    @word = word
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+
+
+end
