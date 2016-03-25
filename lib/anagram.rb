615e5444ac19e018c474bd76981bff2665c345de
jchu4483 <jchu4483@gmail.com>
2016-03-25 16:35:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(test_array)
+    anagram_array = []
+    test_array.map! { |word| word.split("").sort } #=> array of sorted letters
+    test_array.each { |letters| anagram_array << letters == @word.split("").sort }
+    anagram_array
+  end
+
+end
