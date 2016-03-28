705818cf84c67ed6ac82563aecdffec9cb3d564d
miclp <michaelpeimani@gmail.com>
2016-03-27 22:56:58 -0700

    Add #Anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+# anagram.rb
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    matches = []
+    words.each do |word|
+      if word.split("").sort == self.word.split("").sort
+        matches << word
+      end
+    end
+    matches
+  end
+
+end
