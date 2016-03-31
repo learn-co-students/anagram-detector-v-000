8afcc222a5182cee6a236bb8ef785d89f7b313fb
adriandave13 <adrian@paradoxlogic.net>
2016-03-31 18:09:17 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    output = []
+    word_array = word.split("").sort
+    anagrams.each_with_index do |anagram, index|
+      anagram_array = anagram.split("").sort
+        if anagram_array == word_array
+          output << anagrams[index]
+        end
+    end
+    output
+  end
+
+end
