bf9e9b088e985d6dbd8c9c43e45a7d338281b24f
hangloos <ericloos00@gmail.com>
2016-04-07 22:07:28 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+
+    @word = word
+
+  end
+
+def match(array)
+
+new_word = word.split("")
+array.select { |x| x.split("").sort == new_word.sort
+
+}
+end
+
+end
+
+#split the two words and use == to compare them. see if they have the same letters. [1,3,2].sort == [3,2,1].sort = true
