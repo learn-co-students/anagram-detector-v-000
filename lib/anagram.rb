e10f2f51a14857e43e267aaaa7a4110798e55343
Christian Whitesides <whitesidescr@gmail.com>
2016-04-08 17:46:18 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    matching_words = array_of_words.map do |word|
+      if word.split("").sort == @word.split("").sort
+        word
+      end
+    end
+    matching_words.compact
+  end
+end
