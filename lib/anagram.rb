bf92048c4ed615683c87db088ea30824d2553989
kade042 <dereje88@gmail.com>
2016-04-11 00:32:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor = :match
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    new_a = []
+    words.each { |word| new_a << word if word.length == @anagram.length}
+    anagram_a = []
+    new_a.each { |word| anagram_a << word if word.split(/\||/).sort == @anagram.split(/\||/).sort}
+    anagram_a
+    #binding.pry
+  end
+end
