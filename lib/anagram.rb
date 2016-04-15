5956a5e446adba26500f0a4f7ce127101c00f78a
ebanh <ebanhernandez@gmail.com>
2016-04-15 14:18:25 +0900

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(new_word)
+    @word = new_word
+  end
+
+  def match(words)
+    words_by_letter = words.map{|n| n.split("")}
+    anagram_word = @word.split("")
+    
+    words_by_letter.keep_if{|n| n.sort == anagram_word.sort}
+
+    if words_by_letter.empty?
+        words_by_letter
+    else
+      words_by_letter.map{|n| n.join}
+    end
+  end
+
+
+end
