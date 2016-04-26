6ae93bf48c65301f76deadcf3c2d84b8a5897a0c
Michael Walker <michaelwalker610@gmail.com>
2016-04-26 13:11:11 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+  def match(comparison_array)
+    possible_match = []
+    matches = []
+
+		comparison_array.each do |x|
+			possible_match << x
+    end
+
+    possible_match.each do |x|
+      if x.split("").sort.join == @word.split("").sort.join
+        matches << x
+      end
+    end
+		matches
+		end
+
+end
