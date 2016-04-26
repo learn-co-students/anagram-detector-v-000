6a7074b058ec8a024ef89de979d6a51b607634d0
Michael Walker <michaelwalker610@gmail.com>
2016-04-26 13:13:48 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,14 +7,15 @@ class Anagram
 	end
 
   def match(comparison_array)
-    possible_match = []
+    #possible_match = []
     matches = []
 
-		comparison_array.each do |x|
-			possible_match << x
-    end
+		#comparison_array.each do |x|
+			#possible_match << x
+    #end
 
-    possible_match.each do |x|
+    #possible_match.each do |x|
+    comparison_array.each do |x|
       if x.split("").sort.join == @word.split("").sort.join
         matches << x
       end
