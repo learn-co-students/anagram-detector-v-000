c8cde20b0be96ece63c88a494a872071547f3ae6
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2016-04-28 18:51:42 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(sentence)
+	    word_split = @word.split("").sort
+	    sentence.find_all {|sentence_word| sentence_word.split("").sort == word_split.sort}
+	    
+	end
+
+end
+
