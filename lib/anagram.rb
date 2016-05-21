5a137853d1dc9e2a39997e64b8a7e86a83a789f9
Savannah Scott <sascott624@gmail.com>
2016-05-21 17:09:56 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    letters = word.scan(/(.)/).flatten.sort
+    array.keep_if do |random_word|
+      random_word.scan(/(.)/).flatten.sort == letters
+    end
+  end
+
+end
+
+# array.keep_if do |random_word|
