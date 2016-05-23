e6e15be50094f5df4100df812bfdcd52bedda6f9
bshap93 <benihana858@gmail.com>
2016-05-23 11:28:02 -0600

    Anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(match_words)
+    match_words.select do |match_word|
+      match_word.split("").sort == word.split("").sort
+    end
+  end
+
+
+end
