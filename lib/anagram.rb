2b1e45fdba17641c28cab2bf2d9efdb5ae10df4c
Dom McKellar <dom.mckellar@gmail.com>
2016-05-28 10:59:33 -0700

    Refactor #match

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,12 +7,17 @@ class Anagram
   end
 
   def match(word_list)
-    matches = []
-    word_list.each do |word|
-      if word.chars.sort == anagram.chars.sort
-        matches << word
-      end
+  #   matches = []
+  #   word_list.each do |word|
+  #     if word.chars.sort == anagram.chars.sort
+  #       matches << word
+  #     end
+  #   end
+  #   matches
+  # end
+    word_list.select do |word|
+      word.split("").sort == anagram.split("").sort
     end
-    matches
   end
+
 end
