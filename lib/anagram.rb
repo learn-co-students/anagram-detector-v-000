f771705e39352102c1880fc79700e1499208cef4
Caleb Faught <faughtcj@gmail.com>
2016-06-10 19:13:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    anagrams = []
+    possible_anagrams.each do |possible_anagram|
+      letters = possible_anagram.split("")
+      possible = letters.sort!.join("")
+      word = @word.split("").sort.join("")
+      if possible == word
+        anagrams << possible_anagram
+      end
+    end
+    anagrams
+  end
+end
