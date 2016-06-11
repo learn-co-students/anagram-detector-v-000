eb7d4dfcb77c6f31ed24d23e7e6668f147b78de0
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-11 22:48:49 +0000

    earlier versio

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = array.dup.collect do |item|
+      item = item.split("").sort {|a, b| a <=> b}.join("")
+    end
+    word = @word.split("").sort {|a, b| a <=> b }.join("")
+    new_array = new_array.each_with_index.select { |item, index| item == word }
+    [array[new_array[0][1]]] ? [array[new_array[0][1]]] : []
+  end
+end
