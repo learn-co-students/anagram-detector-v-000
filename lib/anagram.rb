e98472b59c6c60a197361a162c868cf3114208cd
Jennifer Dixon <jenniration@gmail.com>
2016-06-17 17:32:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select {|word| word.downcase.chars.sort.join == self.word.downcase.chars.sort.join}
+  end
+end
