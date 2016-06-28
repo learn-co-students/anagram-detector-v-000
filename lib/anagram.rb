cb344e2218a76284fbdbc26a068dad00a3d5612d
Keith Wellman <keith.wellman@icloud.com>
2016-06-28 04:28:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram)
+    match_result = []
+    anagram.each do |word|
+      if word.split("").sort == @word.split("").sort
+        match_result << word
+      end
+    end
+    match_result
+  end
+
+end
