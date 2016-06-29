4599d3f798bc961f0f4e6b6145f9f76c71479662
vicision <duncan.j.mclachlan@gmail.com>
2016-06-29 17:55:02 -0400

    Done: all tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_letters = word.split("").sort
+    matches = []
+    array.each do |w|
+      letters = w.split("").sort
+      if letters == word_letters
+        matches << w
+      end
+    end
+    matches
+  end
+
+end
