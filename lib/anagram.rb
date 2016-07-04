d0f29775bfb71d86ca981be4be14a089efb76531
Adam Taitano <adam.taitano@gmail.com>
2016-07-04 04:26:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  @@match = []
+  def initialize(word)
+    @@word = word
+  end
+
+  def match(array)
+    @@match.clear
+    array.each do |word|
+      letters = word.split(//).sort
+        if @@word.split(//).sort == letters
+          @@match.push(word)
+        else
+        end
+    end
+    @@match
+  end
+
+end
