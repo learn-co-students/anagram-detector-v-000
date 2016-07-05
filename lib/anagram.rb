7ab0f6ffc7985ea09d80f3e072fe9a3b02307e3d
Ben Norris <bennorrismisc@gmail.com>
2016-07-05 12:21:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  attr_accessor :word
+
+  def match(str)
+    grams = []
+    str.each do |x|
+      if x.split('').sort == @word.split('').sort
+        grams << x
+      end
+    end
+    return grams
+  end
+
+end
