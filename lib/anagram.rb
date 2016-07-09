1b0e1d372abe6699a190fdf3df575c68d04c281e
ga-be <gj@mail.co.de>
2016-07-08 21:55:24 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(word)
+    self.word = word
+  end
+
+  attr_accessor :word
+
+  def match(possible_anagrams)
+    split_word = self.word.split(//).sort
+    possible_anagrams.collect do |possible_word|
+      possible_word if possible_word.split(//).sort == split_word
+    end.compact!
+  end
+end
