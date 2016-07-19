24cc5c3767d790864a2bc44df3e2836adbeca0e8
Eric Chu <erickentleongchu@gmail.com>
2016-07-19 02:23:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words.split(' ')
+  end
+
+  def match(word)
+    s_words = @words.split
+  end
+end
