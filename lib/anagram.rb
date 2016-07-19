b3727cfbc4ba223c3eefa6f757e853837a101b21
Eric Chu <erickentleongchu@gmail.com>
2016-07-19 02:28:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,12 @@
 class Anagram
   attr_accessor :words
 
-  def initialize(words)
-    @words = words.split(' ')
+  def initialize(word)
+    @name = word
   end
 
-  def match(word)
-    s_words = @words.split
-  end
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end  
+
 end
