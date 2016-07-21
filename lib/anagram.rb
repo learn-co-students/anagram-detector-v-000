c352f0f1977cae466389c41a02f5127b0a594e9e
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-21 21:11:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_reader :test_word
+
+  def initialize(test_word)
+    @test_word = test_word
+  end
+
+  def match(match_array)
+    return_value = []
+    match_array.each do |match_word|
+      if match_word.split("").sort == @test_word.split("").sort
+        return_value << match_word
+      end
+    end
+    return_value
+  end
+end
