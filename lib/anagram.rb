95133912bc4d6c5344f1570d6844a9fa5cbfae58
William Jones <enquiries@forestfed.com>
2016-08-03 04:14:06 +0000

    find anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+
+    @word = word
+
+  end
+
+  def match(anagrams)
+    anagram_array = []
+    array = []
+    anagrams.each do |anagram|
+      anagram_array = anagram.split("")
+      word_array = @word.split("")
+      if anagram_array.sort == word_array.sort
+        array << anagram
+      end
+    end
+    array
+  end
+end
