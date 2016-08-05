1a3dcdd674e6e04a07505d307eb75433422fec1d
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-05 19:18:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+# Your code goes her
+require 'pry'
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    match = []
+    array.select {|mat| mat.split("").sort == @word.split("").sort}
+  end
+end
