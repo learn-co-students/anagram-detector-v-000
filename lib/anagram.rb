6968c8a2e05bd395b2b140850f1c84f93577f08b
Elias Ventura <eliasventura23@gmail.com>
2016-08-13 00:11:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,12 @@
-# Your code goes here!
 class Anagram
-  attr_accessor :name
+
+  attr_accessor :word
+
   def initialize(word)
-    @name = word
+    @word = word
   end
 
   def match(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
-  end
+     array.select {|x| x.split("").sort == @word.split("").sort}
+   end
 end
