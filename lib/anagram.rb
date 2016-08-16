242137bdd5da0126d0ed44ba1e8a7b450309890a
Binny Lewis <binnylewis@gmail.com>
2016-08-16 17:20:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :match
+
+  def initialize(match)
+    @match = match.scan /\w/
+  end
+
+  def match(words)
+    match_word = []
+    words.each { |word|
+      match_word = word.scan /\w/
+      if match_word.sort == @match.sort
+        #word
+      end
+    }
+    @@match
+  end
+
+end
