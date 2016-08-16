9c711a3787ed25c8dfdb6f2b8bf4b060a75faac5
jinstrider2000 <efrainperezjr@live.com>
2016-08-16 14:27:19 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :init_word
+
+  def initialize(word)
+    self.init_word = word
+  end
+  
+  def match(array)
+    init_word_sorted = self.init_word.chars.sort
+    found_anagrams = array.select {|words| init_word_sorted == words.chars.sort}
+  end
+end
