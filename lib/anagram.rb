14ebf0ba33b212a5a95d40ab4183863f53d17153
John Verdone <jmverdone@gmail.com>
2016-08-15 22:55:55 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+class Anagram
+  attr_reader :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(array)
+
+    matching_word = []
+    array.each do |anaword|
+              if words.split("").sort.join("")  ==  anaword.split("").sort.join("")
+              matching_word <<  anaword
+            else
+              matching_word
+            end
+
+    end
+    matching_word
+
+  end
+
+end
