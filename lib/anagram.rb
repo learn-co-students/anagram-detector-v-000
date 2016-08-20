84212df73ff2a641a78296e43f8c605ae789f333
Malind Nikulski <malind.nikulski@gmail.com>
2016-08-20 15:47:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,17 @@
 class Anagram
-  attr_accessor :name
+  attr_accessor :word
 
-  def initialize(name)
-    @name = name
+  def initialize(test_word)
+    @test_word = test_word
   end
 
   def match(array)
+    anagram = []
+    array.each do |word|
+      if word.split("").sort == @test_word.split("").sort
+        anagram << word
+      end
+    end
+    anagram
   end
 end
