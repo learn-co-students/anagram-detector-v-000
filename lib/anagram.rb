e5ee82a482a68d1f736e9635c6cd2731a1702cfa
Kenlyn Terai <kkterai@gmail.com>
2016-08-21 18:21:44 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,13 +6,6 @@ class Anagram
   end
 
   def match(word_list)
-    anagram_list= []
-      word_list.each do |anagram|
-        if anagram.split("").sort == word.split("").sort
-          anagram_list.push(anagram)
-        else
-        end
-      end
-    anagram_list
+      word_list.select {|anagram|  anagram.split("").sort == word.split("").sort}
   end
 end
