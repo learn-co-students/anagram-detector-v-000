6c6049d38dba346cabceb27957b99785552cdd04
vanita eckert <vanita112003@gmail.com>
2016-09-16 00:49:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select do |each_word|
+      each_word = each_word.split(//)
+      if result = each_word.sort == @word.split(//).sort
+        result
+      end
+    end
+  end
+
+end
