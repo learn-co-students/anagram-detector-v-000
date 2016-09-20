b3af198c4e9bebc954af8ad1823513cafb114855
bc86 <michael.4486@gmail.com>
2016-09-20 12:19:26 -0400

    check for anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagram) #Method to compare words for anagrams
+    possible_anagram.select { |anagram| anagram.split("").sort == word.split("").sort }
+  end
+
+end
