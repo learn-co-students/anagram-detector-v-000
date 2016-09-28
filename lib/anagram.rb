da813dc2d58586eeb7b66b876ab732edd4217b8c
Chirag Dudhat <cdudhat@gmail.com>
2016-09-28 04:11:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(query)
+    result = [] #Assign blank array to the result in case there are no matching values
+    query.each do |x| #Iterate over each word from the query array to match it with the current word
+      result << x if x.split(//).sort == @word.split(//).sort
+    end
+    result
+  end
+end
