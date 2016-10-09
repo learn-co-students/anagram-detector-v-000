ed21c04711cc62a6f82d8b8707a1b687ae716686
Juan Gongora <gongora.animations@gmail.com>
2016-10-09 22:54:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word)
+    original = @word.split("").sort
+    collection = []
+    word.select {|x| collection << x}
+    collection.select {|item| original == item.split("").sort}
+  end
+
+end
