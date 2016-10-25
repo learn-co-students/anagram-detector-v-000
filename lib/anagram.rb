578afe31416fa589b17c40a8901ae8b5f59c1fa4
Jon Fougner <jon.fougner@gmail.com>
2016-10-25 16:07:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @@word = word
+  end
+
+  def match(possible_anagrams)
+    matches = []
+    possible_anagrams.each do |possible_anagram|
+      matches << possible_anagram unless possible_anagram.split("").sort != @@word.split("").sort
+    end
+    matches
+  end
+
+end
