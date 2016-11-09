4690ab18d755839015f3862641e55026d4157ca3
Pia Kim <pkim2320@gmail.com>
2016-11-09 23:12:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |string|
+      letters = string.split("").sort
+      word.split("").sort == letters
+    end
+    end
+
+  end
