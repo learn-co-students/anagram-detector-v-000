dbc9ba38e7e9ad8f878e4195474d307619fdc9c9
John Pfingst <jpfingst@outlook.com>
2016-11-15 03:32:56 +0000

    anagram finder

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(match_word)
+    @match_word = match_word.split('').sort
+  end
+
+  def match(words)
+    matches = []
+    words.collect { |word|
+       word.split('').sort == @match_word ? matches << word : nil
+     }
+     matches
+  end
+end
