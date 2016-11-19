48ebe2f91401447a2b5ffccad6a0f4f157f17b3a
Dequan Xiao <splscoupon@gmail.com>
2016-11-19 08:47:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    found_word = []
+    words.each do |this_word|
+      if this_word.split("").sort == @word.split("").sort
+        found_word << this_word
+      end
+    end
+    found_word
+  end
+end
