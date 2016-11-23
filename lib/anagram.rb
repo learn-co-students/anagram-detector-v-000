6c8ea1b7d24ec9c015ae3759a1c83c7e8e8adf14
Tyson Philips <sangdencre@gmail.com>
2016-11-23 21:51:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word, :anagram_sort
+
+  def initialize(word)
+    @word = word
+    @anagram_sort = word.chars.sort.join
+  end
+
+  def match(word_list)
+    anagram = self.anagram_sort
+    anagram_list = []
+    word_list.each do |word|
+      if word.chars.sort.join == anagram
+        anagram_list << word
+      end
+    end
+    anagram_list
+  end
+end
