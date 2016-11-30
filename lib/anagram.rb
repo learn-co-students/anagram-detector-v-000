be136ff1dc11ac312955f800b906ede60fe289db
dwyn <dwayne.harmon@gmail.com>
2016-11-30 00:44:09 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  @@word_list = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arg)
+    arg.select do |n| 
+      n.split("").sort == word.split("").sort
+    end
+  end
+end
+
+
+#
+# def match(word)
+#   word1 = word.split.sort
+#   word2 = []
+#
+#
+#   @@word_list.each do |werd|
+#     word2 = werd.split.sort
+#     if word1 == word2
+#       return true
+#     end
+#   end
+# end
