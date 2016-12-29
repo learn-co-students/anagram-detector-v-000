9d92656bfa9c370245f9d1b5112fa0107b9ea6f2
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2016-12-29 23:47:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize (word)
+    @word =word
+  end
+def match(word)
+  word.select do |x|
+if x.split("").sort == @word.split("").sort
+word
+end
+end
+  end
+end
