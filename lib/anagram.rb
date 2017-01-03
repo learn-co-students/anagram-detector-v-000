746fa59112470d2f1e86f7322ac7b3163bc1a267
Elvin <elvin@elvingarcia.com>
2017-01-03 00:31:22 -0500

    add

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+    def initialize (word)
+      @word = word
+    end
+
+    def match (arr)
+      # splits each letter
+      # then sorts them in alphabetical order
+      @word = @word.split("").sort
+      #Compares each object in the array length against @word
+      #deletes them if statement is true . results in same length words
+      arr = arr.delete_if {|obj| obj.length != @word.length}
+      #take the remaining words and compares them sorted
+      #it is first split inorder to sort them
+      arr.delete_if { |obj| obj.split("").sort != @word }
+      #returns the remaining words array
+      arr
+    end
+
+end
