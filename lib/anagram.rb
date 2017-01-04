265e527b55651483624acb1ff60dda751ab294c2
Daniel Goldberg <danpaulgo@aol.com>
2017-01-04 07:35:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor
+
+  def initialize(word)
+    @word = word
+    @letters = word.split("").sort
+  end
+
+  def match(array)
+    array.select do |word|
+      word.split("").sort == @letters
+    end
+  end
+
+end
