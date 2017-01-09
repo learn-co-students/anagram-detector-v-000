821f228c046b04a41ff53f65cdbf2b46056b6660
Jonathan Foster <redcabinny@gmail.com>
2017-01-09 03:23:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(anagram_array)
+    anagram_array.select do |anagram_candidate|
+      self.anagram.split("").sort == anagram_candidate.split("").sort
+    end
+  end
+
+end
