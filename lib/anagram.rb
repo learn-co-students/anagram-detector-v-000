63aa8873d132fd739dbaf18cd97ed0c780316a4b
ryanpmcmahon <ryan@mcmahon.work>
2017-01-12 18:50:29 -0500

    added initialize and match methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word.split("")
+  end
+
+  def match(word_array)
+    new_arr = word_array.collect{|x| x.split("")}
+    new_arr = new_arr.collect{|x| x.sort!}
+    new_arr.find_all{|x| x == word.sort}
+  end
+
+end
