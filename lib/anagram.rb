90a07e196141827cc72fbd97d9c2b85e6b64cd91
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-15 13:06:38 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,17 +6,8 @@ def initialize(word)
   @word = word 
 end# of initialize
 
-
 def match(array)
-  answer = []
-  array.each do |word|
-    letters = word.split("")
-    inquiry = @word.split("")
-    if letters.sort == inquiry.sort 
-       answer << word 
-    end# of if 
-  end# of do
-  return answer 
+ array.select {|word| word.split("").sort == @word.split("").sort}
 end# of match
 
 
