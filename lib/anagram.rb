5cbba3d2f96807a2e07bb5c7a4eb739effbb311d
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-15 12:22:33 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word 
+end# of initialize
+
+
+def match(array)
+  answer = []
+  array.each do |word|
+    letters = word.split("")
+    inquiry = @word.split("")
+    if letters.sort == inquiry.sort 
+       answer << word 
+    end# of if 
+  end# of do
+  return answer 
+end# of match
+
+
+end# of class
