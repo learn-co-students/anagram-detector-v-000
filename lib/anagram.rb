7181d7d28118421322bc4265bbf3de47fa8e4403
Kristi Thompson <kristiforpresident@gmail.com>
2017-01-21 21:27:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word.split("")
+  end
+
+  def match(words)
+    matching_words = []
+
+    words.each do |word|
+      test_against = word.split("")
+      if test_against.sort == @word.sort
+        matching_words << word
+      end
+    end
+
+    matching_words
+  end
+
+end
