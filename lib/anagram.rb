970f72711c224b693e9d5bd3a65d59b0746c40b5
Rachel Lim <rslim87@gmail.com>
2017-01-29 17:50:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+	attr_reader :anagram
+
+	def initialize(anagram)
+     @anagram = anagram
+   end
+
+	 def match(array)
+		 new_array = []
+			array.each do |word|
+				if word.split("").sort == anagram.split("").sort
+					new_array << word
+				end
+			end
+			new_array
+		end
+
+
+
+
+
+
+end
