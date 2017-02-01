d7f638883b27fe157076ab8c36554466131d8fcb
Damien de Lartigue <damien@delartigue.co.uk>
2017-02-01 20:06:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_arr)
+    new_arr = []
+    test_word = @word.split('').sort.join('')
+    word_arr.each do |word|
+      list_word = word.split("").sort.join("")
+      #binding.pry
+      if test_word == list_word
+        new_arr << word
+      end
+    end
+    new_arr
+  end
+
+  
+end#end of class
