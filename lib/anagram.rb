a2fb8667c72bb506ba8031d35c91dc53352f68a2
charlie lee <ctlee9096@gmail.com>
2017-02-14 04:32:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+#require 'pry'
+class Anagram
+  #@@all = []
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  def match(names)
+    #binding.pry
+    #select method does? returns an Enumerator
+     names.select do |value|
+     #calls on chars method to return an array of character
+     #and chain the sort(alphabetically when there are
+     #no arguments) method
+     #pass the message to receiver, value
+     value.chars.sort == @anagram.chars.sort
+    end
+  end
+end
+#https://gist.github.com/davidbella/6867249
