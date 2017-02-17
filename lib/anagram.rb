ecccbfb7d8a69a0ed44ad6fd439f24a59a59e7e8
Erika Hughes <ehughes@knights.ucf.edu>
2017-02-17 02:20:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    matches = []
+    word_split = @word.split("")
+    words.each do |w|
+      if w.split("").sort == word_split.sort
+        matches << w
+      end
+      # binding.pry
+    end
+    matches
+  end
+end
