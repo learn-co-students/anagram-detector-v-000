f2c5113f120ce1a750dea12d5b3a99cf4c42f66e
Mason Ellwood <masonellwood@gmail.com>
2017-02-22 21:36:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+      @word = word
+  end
+
+  def match(words)
+    matches = []
+    word_split_up = @word.split("")
+    words.each do |i|
+      if i.split("").sort == word_split_up.sort
+        matches << i
+      end
+    end
+    matches
+  end
+end
