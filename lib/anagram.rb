dfe03bd0e054b3565949271aa601d0ba1651c9ea
Jessica Widman <jawidman.30@gmail.com>
2017-02-23 02:09:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :world
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    anagrams = []
+    words.each do |compare_word|
+      if compare_word.split("").sort == @word.split("").sort
+        anagrams << compare_word
+      end
+    end
+    anagrams
+  end
+
+end
