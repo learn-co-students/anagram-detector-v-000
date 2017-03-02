ef115d437a57f6710f9c9c8321758a8f684c663e
bortch89 <bortch@gmail.com>
2017-03-02 12:09:15 +0200

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  @@all = []
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @@all << self
+  end
+
+  def match(array_of_anagrams)
+    array = array_of_anagrams
+    sorted_word = @word.chars.sort.join
+    result = []
+    array.find_all do |anagram|
+      sorted_anagram = anagram.chars.sort.join
+      sorted_word == sorted_anagram
+    end
+  end
+end
