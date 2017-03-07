6d14c611c5a7d895cd951e7007974efdde234c24
Isis Tejeda <isis@isistejeda.com>
2017-03-07 12:14:52 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(many_words)
+    second_word = word.split("").sort
+
+    many_words.select do |palabra|
+      palabra.split("").sort == second_word
+
+    end    #.["w", "o", "r", "d"]
+
+
+  end
+
+end
