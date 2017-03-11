20ace48aa32f7f53b06999b378123a4e8fe0b2fe
Michael Etlinger <mk.etlinger@gmail.com>
2017-03-11 19:20:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |word|
+      word.chars.sort.join == @word.chars.sort.join
+    end
+  end
+
+end
