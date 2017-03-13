87aef5ca48f234e7037876b3c46efb34be2763a5
Hima Chitalia <hima_chhag@yahoo.com>
2017-03-13 21:12:15 +0000

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matching_word = []
+    array.select do |words|
+      letters = words.split("").sort
+      word_two = word.split("").sort
+      if letters == word_two
+        matching_word << words
+      end
+    end
+    matching_word
+  end
+
+end
