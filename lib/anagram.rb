67b0c97fefa1341d9ff22fb033f487699d48eaeb
Nadine Fischoff <nadine.khattak@gmail.com>
2017-03-15 10:37:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(words)
+    letter_array = []
+    letters_words = []
+    wordsMatch = []
+    letter_array = @word.split('')
+
+    words.each do |aword|
+      letters_words = aword.split("")
+      if letters_words.sort == letter_array.sort
+        wordsMatch << aword
+      end
+    end
+
+    wordsMatch
+
+
+
+  end
+
+end
