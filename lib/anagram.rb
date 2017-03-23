cb18c3d95281625ca1e0c69088699e297e87ab03
Sergio Mendoza <smendoza787@gmail.com>
2017-03-23 03:14:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_array)
+    split_word = @word.split("")
+    anagrams = []
+    anagram_array.each do |anagram|
+      split_anagram = anagram.split("")
+      if split_anagram.sort == split_word.sort
+        anagrams << anagram
+      end
+    end
+    anagrams
+  end
+end
