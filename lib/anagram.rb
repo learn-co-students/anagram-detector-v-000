82b6e2dbc16c3a333fcf2a5b598f506a556ead81
Terry Blue <terryblue@comcast.net>
2017-03-24 19:31:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+	attr_accessor :Anagram
+
+	def initialize(anagram)
+		@anagram = anagram
+		@@anagrams = []
+	end
+
+	def match(array)
+		@anag_sorted = @anagram.chars.sort(&:casecmp).join # case insensitive sort of a string
+		array.each do |word|
+			@word_sorted = word.chars.sort(&:casecmp).join
+			if @word_sorted == @anag_sorted
+				@@anagrams << word
+			end
+		end
+		@@anagrams
+	end
+
+end
