5fb1d40f1aa65a551c9a5ddf684c7a79677c2e6f
Laamia Islam <laamia.islam@gmail.com>
2017-03-25 20:03:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def split_and_sort(word)
+    word.split("").sort
+  end
+
+  def match(array)
+    result = []
+    array.each do |array_word|
+      if split_and_sort(array_word) == split_and_sort(@word)
+        result << array_word
+      end
+    end
+    result
+  end
+end
