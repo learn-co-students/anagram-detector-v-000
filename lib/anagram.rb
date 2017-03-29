e4af833c80f12d961e0fb570165121bd0d0c91b4
William Barela <william.barela.dev@gmail.com>
2017-03-29 15:59:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  attr_reader :test_word
+
+  def initialize(test_word)
+    @word = test_word
+    @test_word = order_string(@word)
+  end
+
+  def match(anagram_list)
+    anagram_list.find_all{|element| order_string(element) == @test_word}
+  end
+
+  def order_string(word)
+    word.split(//).sort.join
+  end
+end
