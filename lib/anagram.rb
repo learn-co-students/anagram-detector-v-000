ced16943528731e785ec5e43bda9fd5fe1ec5b60
Andrew Bonner <andrewbonner92@gmail.com>
2017-03-31 15:33:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(potential_anagrams)
+    anagrams = potential_anagrams.select {|word| word.chars.sort == @word.chars.sort}
+  end
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+
+# => ["inlets"]
