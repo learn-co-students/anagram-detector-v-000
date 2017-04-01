766f728a24ec330c44f766d6a51a421f01989b21
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-01 18:52:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    no_matches = []
+    array.each do |word|
+      word.split(",").sort == @word.split(" ").sort
+    # binding.pry
+    end
+  end
+
+end
