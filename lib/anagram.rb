69366b731124823a7d9b0e51b41ccfacc8761ce4
Daniela Carvajal <d.carvajal.agama@gmail.com>
2017-04-02 17:56:21 +0000

    done anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words, :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+    @words = words
+  end
+
+
+  def match (words)
+    words.select do |word|
+      word.split(//).sort == @anagram.split(//).sort
+  end
+end
+
+#if (w2.downcase.split(//).sort == w1.downcase.split(//))
+#word.max{|a,b| a.length <=> b.length} 
+
+end
