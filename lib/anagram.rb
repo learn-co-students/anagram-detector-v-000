193ec58650bc4b7f8b7deb1c2b3f37bf942acb4f
Carolyn Yates <yatescjnd@gmail.com>
2017-04-03 20:33:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_list) #list of words in an array that we are comparing the initialized word t
+    anagram_list.select {|anagram| anagram.split("").sort == word.split("").sort }
+    #OR
+    # new_list = []
+    # anagram_list.map do |anagram|
+    #   if anagram.split("").sort == word.split("").sort
+    #     new_list << anagram
+    #   end
+    # end
+    # new_list
+  end
+end
