f12cf90f5b6a384c22d7472793f205b21c887d5c
Eva Navarror <emanao@me.com>
2017-04-07 17:25:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+
+require "pry"# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word=word
+  end
+  def match (anagram_array)
+    result= []
+    word_chars = word.split(//).sort
+    anagram_array.each do |anagram|
+      anagram_chars = anagram.split(//).sort
+      result <<anagram if word_chars == anagram_chars
+    end
+    result
+  end
+end
