e2278d670b19d4d4b92ad44d0e072f987ef60ff0
Leonardo Guzman <lguzman@me.com>
2017-04-12 03:45:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+      #select the words, the .select method is what prompts the return of the matching words
+     array.select do |words|
+       #going to select the words that match and return them
+      words.split("").sort == @word.split("").sort
+     end
+    end
+
+
+end
