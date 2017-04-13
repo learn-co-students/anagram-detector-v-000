020740efbdc578326c8a64d035712a559de0333f
jose cobos <luiscobosj@gmail.com>
2017-04-13 21:10:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    array_of_words.select {|match| match.chars.sort.join == @word.chars.sort.join}
+  end
+
+end
