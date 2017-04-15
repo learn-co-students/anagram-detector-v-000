b59b5397b52516f07f09838cd58dbc1cd8277960
David Mars <david.w.mars@my.occc.edu>
2017-04-15 05:44:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+# ~~~HAVICK WAS HERE
+
+class Anagram
+attr_accessor :word, :list
+def initialize(word)
+@word = word
+end
+
+def match(list)
+  @list = list
+
+  list.map {|word| word.split("")}.find_all{|array| array.sort == word.split("").sort}.map {|array| array.join}
+
+
+  end
+end
