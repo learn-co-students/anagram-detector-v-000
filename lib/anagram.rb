7114e3b1be955620094551a4fd0756e049f97770
rosiemin <rosiemin11@gmail.com>
2017-04-16 22:12:32 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    ana = []
+    list.collect do |word|
+      if word.chars.sort == @word.chars.sort
+        #char.sort will sort the characters within the string we are calling that method on.
+        ana << word #will insert the word if the above is true, this will collect all instances within the array that come back as true
+      end
+    end
+    ana #returns the array 
+  end
+
+
+
+end
