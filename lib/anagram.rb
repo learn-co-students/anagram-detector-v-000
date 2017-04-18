426ca85c55e253d4faecfa548a8bdcea25fe36b6
Joseph Hyatt <jhyatt1017@gmail.com>
2017-04-18 22:18:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+#should detect no matches
+#should detect a simple anagram
+#should detect an anagram
+#should detect multiple anagrams
+class Anagram
+
+	attr_reader :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(anagrams)
+		anagrams.select { |anagram| anagram.split("").sort == @word.split("").sort }
+	end
+end
