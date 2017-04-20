dabc15c715e296fd4a16e2d2d163528e8f5902ea
natasha kelly <kellynatashav@yahoo.com>
2017-04-20 14:35:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |string| # select will return matching set or emply array for false eval
+     string.split(",") # split initial string into single words
+      char_array = string.split('') # split into characters
+       split_words = word.split('') # split word initialize variable into characters
+      char_array.sort == split_words.sort # compare the two arrays for matching characters
+    end
+  end
+
+
+end
