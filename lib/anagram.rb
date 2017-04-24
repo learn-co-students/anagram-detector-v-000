8f422ed4378ca8cabff127e2c4c52652e51e31e0
Alfredo Urdaneta <alfredo.i.urdaneta@gmail.com>
2017-04-24 14:28:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    arr = []
+    letters_arr = @word.split("")
+    array.each do |word|
+      new_arr = word.split("")
+      if new_arr.sort == letters_arr.sort
+        arr << word
+      end
+    end
+    arr
+  end
+
+end
