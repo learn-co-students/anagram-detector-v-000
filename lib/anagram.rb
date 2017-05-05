2e5d4b7529bc6baf2373dbdc8ca15286d0d175d5
Lia HUANG <lisa.yc.huang@gmail.com>
2017-05-05 12:13:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_arr)
+    word_arr.find_all {|word|   #>  use find_all method to return an array
+      word.split("").sort == @word.split("").sort} #>Split each word into alphabets, sort and compare
+  end
+
+end
