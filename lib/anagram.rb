b3119dabd19cc379cb57e3caec9604718476dcf1
Christina Varipapa <christina.varipapa@gmail.com>
2017-05-08 01:28:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(potential_matches)
+    potential_matches.select do |potential_match|
+      potential_match.split("").sort == @word.split("").sort
+    end
+  end
+
+end
