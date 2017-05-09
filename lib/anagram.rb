b7e29677130835a4c8b1ee20096c46e90ce0e7f9
Rebecca Bratu <beckybratu@gmail.com>
2017-05-09 02:38:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(words_array)
+  solution = []
+  words_array.each do |w|
+    if w.split("").sort == @word.split("").sort
+      solution << w
+    end
+  end
+  solution
+end
+
+end
