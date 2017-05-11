2bd39b86ac661330a1d100a5fe1729e26e85ae6a
Elias Ventura <eliasventura23@gmail.com>
2017-05-11 22:16:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,14 @@
 class Anagram
 
   attr_accessor :word
+
   def initialize(word)
     @word = word
   end
 
   def match(array)
-     array.select {|x| x.split("").sort == @word.split("").sort}
-   end
+    array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
 end
