82e0db4e71ea7d20894a0675442f9611560f68fa
Graham Squires <graham.squires3@gmail.com>
2017-05-16 18:23:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @name = word
+    
+  end
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+end
+
+end
