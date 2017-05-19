9e623185bdd9d702927ee3669b750317bbda9735
Menachem Hornbacher <me@mendel.tech>
2017-05-19 17:55:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram.split("").sort
+  end
+
+  def match(words)
+    words.select{ |word| word.split("").sort == @anagram }
+  end
+end
