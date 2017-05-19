4e760f9c29398f17a68201ac1bd101fec48980b1
Emily Sugano <emily.sugano@gmail.com>
2017-05-19 20:18:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize word
+    @word = word
+    @@user_word = word
+  end
+
+  def match(array)
+    winner = []
+    array.each do |each_word|
+      winner << each_word if each_word.split("").sort == word.split("").sort
+    end
+    winner
+  end
+
+end
