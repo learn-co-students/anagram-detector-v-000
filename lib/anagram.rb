c94de696e0f1da04ff1a6b580b38977e4328d671
Noah Summers <summers.noah@gmail.com>
2017-05-22 19:53:51 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :key_word
+  
+  def initialize(word)
+    @key_word = word
+  end
+
+  def match(array)
+    array.select do |word|
+      @key_word.scan(/\w/).sort == word.scan(/\w/).sort
+    end
+  end
+  
+end
