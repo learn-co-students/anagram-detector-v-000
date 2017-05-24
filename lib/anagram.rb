e8f6b386a4fe54d938754574bd89733b4697bb45
Samson Yuwono <samson.yuwono@gmail.com>
2017-05-24 19:27:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    #iterate over array of words
+    #if array matches word return words in array form
+    #split each word in array input and split word into characters
+    matches = []
+    array.each do |each_word|
+        if each_word.split("").sort == word.split("").sort
+        matches << each_word
+      end
+      end
+    matches
+    end
+
+
+
+end
+
+#input for
