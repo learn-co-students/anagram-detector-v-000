acfbc0d5de835d880f57759d4f3cec511a06d559
Sullivan Nolan <nolakat@gmail.com>
2017-05-24 07:59:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+
+#call class 
+class Anagram
+attr_accessor :baseword 
+
+def initialize(word)
+#hoist variable
+@baseword = word 
+end 
+
+#call match method 
+def match(testwords)
+
+  #return words which match testwords
+  testwords.select do |word| 
+    #split up words into letters
+    letters = word.split('')
+    #sort letters and baseword into alphabetical order, do they match?
+    letters.sort == baseword.chars.sort 
+  end 
+end 
+
+end  
