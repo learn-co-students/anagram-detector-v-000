93e40ddc9a1f2a09fdaa26d1872d9dd3a05038b7
gb23 <gregbenj@hotmail.com>
2017-05-25 16:05:12 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(potential_anagrams)
+        answer_array = []
+        word_letters_sorted = @word.split("").sort
+        potential_anagrams.each{ |word|
+           answer_array << word if word.split("").sort == word_letters_sorted
+        }
+        answer_array
+    end
+end
