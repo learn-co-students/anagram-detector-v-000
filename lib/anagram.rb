b486470616b275db44a3b04e70919d4cbf29c3db
zoebisch <zoebisch@gmail.com>
2017-05-25 17:44:58 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Anagram
+    attr_reader :anagram
+
+    def initialize(definition)
+      @anagram = definition
+      @output = {}
+      @output[:anagram] = []
+    end
+
+    def match(list)
+      list.each do |item|
+          save = []
+          if item.length == @anagram.length #unequal lengths are NO TEST
+            item.split(//).each do |letter|
+              @anagram.include?(letter) && item.count(letter) == @anagram.count(letter) ? save << letter : nil
+            end
+          end
+          
+          if item == save.join
+            @output[:anagram] << save.join
+          end
+
+      end
+      @output[:anagram]
+    end
+
+end
