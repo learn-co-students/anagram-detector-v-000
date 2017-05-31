af033cd7315e3bb78b37cd04a43d4e52040ee57f
Jenni Macklin <jennimacklin@hotmail.com>
2017-05-31 16:10:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    matching_words = []
+    letters = @word.split("")
+    list.each do |word|
+      word_letters = word.split("")
+      if word_letters.sort == letters.sort
+        matching_words << word
+      end
+    end #end of list do loop
+    matching_words
+  end #end of match method
+
+end
