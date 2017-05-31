3bcae314a674d4ad5c4d6f2fed678134777b76f3
Luisa Scavo <luisascavo@gmail.com>
2017-05-31 18:30:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams_potential)
+    anagrams_potential.select do |x|
+      x.split("").sort == @word.split("").sort 
+    end
+  end
+end
