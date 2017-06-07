1be9df12b28b51a9e2a0be7e91cb0ef5372f88b5
Ninian Bond <gentile.bond@gmail.com>
2017-06-07 15:57:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  def match(possible_anagrams)
+    result = []
+    sorted_answer = @word.chars.sort.join
+    possible_anagrams.each do |test_word|
+      temp_word = test_word.chars.sort.join
+      if temp_word == sorted_answer
+        result << test_word
+      end
+    end
+    result
+  end
+end
