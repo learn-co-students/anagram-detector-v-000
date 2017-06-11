5b9ce1924738c454f5a7e7ec535ac75401b01422
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-11 13:50:19 +0000

    Built Class Anagram, first solution to #match finished

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    match_array = []
+    word_sorted = @word.split("").sort
+    word_list.map do |word|
+      match_array << word if word.split("").sort == word_sorted
+    end
+    match_array
+  end
+
+end
