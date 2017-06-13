5e83a6af53a9367804c1e0ce12ba063fb2270f42
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-06-13 18:13:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    list = []
+    words.each do |word_1|
+      if word_1.split("").sort == word.split("").sort
+        list << word_1
+      end
+    end
+  end
