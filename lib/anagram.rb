1b26cbc041ae806e4e37016f64c269a58059c82b
Tyler Gotto <tylergotto17@gmail.com>
2017-06-14 13:45:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+    #is word an anagram with any words in this array?
+    anagrams = []
+    array.each do |wrd|
+      word_array = wrd.split("")
+      og_array = @word.split("")
+      if og_array.sort == word_array.sort
+        anagrams << wrd
+      end
+    end
+    return anagrams
+end
+
+
+end
