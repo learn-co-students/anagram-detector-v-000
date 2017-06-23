0d037aaf11454f2074949900eb9e5ff56b95088f
jamiegiuliano <jamiepgiuliano@gmail.com>
2017-06-23 03:54:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    
+    attr_accessor :word
+    
+    def initialize(word)
+        #should take a word on initialization
+        @word = word
+    end
+    
+    def match(possible_anagrams)
+        possible_anagrams.select do |possibility|
+            possibility.split("").sort == @word.split("").sort
+        end    
+    end
+ 
+end
