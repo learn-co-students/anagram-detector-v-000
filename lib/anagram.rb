02eee96750bba2ab395e01111f9a164c338a251b
Igor Eskin <eskinhd@gmail.com>
2017-06-25 18:14:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = []
+    array.each { |el| new_array << el if el.chars.sort == @word.chars.sort }
+    new_array
+  end
+
+
+end
