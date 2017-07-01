7ff26017efa9d3573b146777a385b9893d73dd06
Pamela Lee <pamela.jean.lee@gmail.com>
2017-07-01 22:37:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+    @match = []
+  end
+
+  def match(anagrams)
+    anagrams.each do |anagram|
+      if @words.split("").sort.join == anagram.split("").sort.join
+        @match << anagram
+      end
+    end
+    @match
+  end
+end
