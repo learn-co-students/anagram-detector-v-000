3ca4d78b4f6c995b32b1d09fe2b29e115c1a5fd5
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-07-02 18:36:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+      @word = word
+    end
+    
+    def match(gram)
+        og = @word.split("").sort
+        gram.select do |word|
+          og == word.split("").sort
+        end
+      end
+    end
