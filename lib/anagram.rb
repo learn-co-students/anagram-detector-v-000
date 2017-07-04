d952f60a3f7e9144121d1e84924df72453bea37e
Sydney Chun <schun08@gmail.com>
2017-07-04 14:19:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,24 @@
+# first attempt 6/22/17
+# class Anagram
+#   attr_accessor :word
+#
+#   def initialize(word)
+#     @word = word
+#   end
+#
+#   def match(list)
+#     letters = @word.split("").sort
+#     result = []
+#     list.each do |item|
+#       if item.split("").sort == @word.split("").sort
+#         result << item
+#       end
+#     end
+#     result
+#   end
+# end
+
+# second attempt 7/4/17
 class Anagram
   attr_accessor :word
 
@@ -6,13 +27,17 @@ class Anagram
   end
 
   def match(list)
-    letters = @word.split("").sort
-    result = []
-    list.each do |item|
-      if item.split("").sort == @word.split("").sort
-        result << item
-      end
+    # result = []
+    # list.each do |item|
+    #   if word.split("").sort == item.split("").sort
+    #     result.push(item)
+    #   end
+    # end
+    # result
+    # refactor: using an iterator to build the array
+    list.select do |item|
+      item.split("").sort == word.split("").sort
     end
-    result
   end
+
 end
