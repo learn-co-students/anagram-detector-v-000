6b8e6e21a9ba96ade8673cf9e2200d7b0e8aacfe
Scyntre <github email address>
2017-07-07 04:08:55 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def sorted_word_array
+		@word.split("").sort
+	end
+
+	def match(word_array)
+		anagram = word_array.map do |w|
+			if w.split("").sort == sorted_word_array
+				w
+			end
+		end
+		anagram.compact
+	end
+end
+
+#change @word into an array of each letter
+
+#take in an array of words
+#go through each word
+#split each word into a new array of each letter
+#check to see if it is equal to the sorted array of @word
