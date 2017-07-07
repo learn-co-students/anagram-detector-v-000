903f89df6f065c3cb481fcddf1897abbca19c3c0
Scyntre <github email address>
2017-07-07 04:09:18 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,10 +18,3 @@ class Anagram
 		anagram.compact
 	end
 end
-
-#change @word into an array of each letter
-
-#take in an array of words
-#go through each word
-#split each word into a new array of each letter
-#check to see if it is equal to the sorted array of @word
