06f42ddf2172a9e7ee8201ea508c6944a7280e21
Kevin Powell <kevdpow@gmail.com>
2017-07-07 20:16:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word_list, :word_to_match
+
+  def initialize(word_to_match)
+    @word_to_match = word_to_match
+  end
+
+  def match(word_list)
+    @word_list = word_list
+    match = []
+    word_list.map do |x|
+      if x.split("").count == word_to_match.split("").count
+        if x.split("").uniq.sort == word_to_match.split("").uniq.sort
+          match << x
+        end
+      end
+    end
+    match
+  end
+
+end
