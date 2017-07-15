012684f144fa71b8e875420dbcf7e9b8e20485e4
Aellon Krider <aellonkrider@gmail.com>
2017-07-15 00:53:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  @@all = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |potential_anagram| potential_anagram.split("").sort
+    @word.split("").sort == potential_anagram.split("").sort #if the word matches a word in the array of anagrams, return the match (via select)
+    }
+  end
+
+end
