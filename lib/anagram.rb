93a9243b8531669db093bdbe82d8ffb068b822f0
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-07-17 22:31:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,12 +4,14 @@ class Anagram
     # Your class, Anagram should take a word on initialization, and instances 
 
      def initialize(word)
-         
+       @word = word
      end
      
      def match([])
+          if array.match
           # it should take an array of possible anagrams and return all matches in an array.
           # if none exist it should return an empty array
+          #iterate over the array after spliting and sorting to an array of letters it a-z to compair it. 
      end
      
 end
