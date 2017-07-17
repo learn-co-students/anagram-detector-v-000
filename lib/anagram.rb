562eab0aa777cc022560c482c04047d0e6178a6d
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-07-17 21:59:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+     attr_accessor :word
+    # Your class, Anagram should take a word on initialization, and instances 
+
+     def initialize(word)
+         
+     end
+     
+     def match([])
+          # it should take an array of possible anagrams and return all matches in an array.
+          # if none exist it should return an empty array
+     end
+     
+end
