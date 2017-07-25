3a1e3714c227847cc99cd93ff1d8f7c6ca76e56d
James Randolph <james.randolph2016@gmail.com>
2017-07-25 23:29:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :phrase
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def match(words)
+    words.select do |word|
+    word.split("").sort == @phrase.split("").sort
+        end
+    end
+
+end
