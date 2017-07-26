cd21abcad3979e3878f389fca5b8b50810ebc500
Erin Land <erinland05@gmail.com>
2017-07-26 00:26:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+  @word = word
+  end
+
+  def match(p_matches)
+    matches = []
+    word_letters = @word.split("").sort
+    p_matches.each do |p_word|
+      if p_word.split("").sort == word_letters
+        matches << p_word
+      end
+    end
+    matches
+  end
+end
