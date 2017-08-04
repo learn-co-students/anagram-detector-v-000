d024ded296d598677ccc84ed66e445870b56a180
Donna <github email address>
2017-08-04 11:43:44 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+	def initialize(word)
+		@word = word
+	end
+
+	def match(possible)
+		letters = @word.split("").sort
+		possible.select do |item|
+			item.split("").sort == letters
+		end
+	end
+end
