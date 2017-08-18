f391ecbbc3ded7ea10e9fdb6687d6ceece245a49
Alexandra Wright <superbiscuit@gmail.com>
2017-08-18 21:56:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_reader :target
+
+  def initialize(target)
+    @target = target
+  end
+
+  def match(array)
+    array.select do |word|
+      word.split('').sort == target.split('').sort
+    end
+  end
+
+end
