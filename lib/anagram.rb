eaa384921d470175ac4d3fa658e8a5da7259f2b5
Archana Burra <burra.archana@gmail.com>
2017-08-18 15:28:36 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    word_list.select do |word|
+      anagram?(word)
+    end
+  end
+
+  def anagram?(word)
+    ana = @word.length == word.length && @word.chars.all? {|c| word.include?(c)}
+    ana
+  end
+
+end
