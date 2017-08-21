6a425bb54dbe40645a7c4970babc0a48ab2e69f0
github username <github email address>
2017-08-21 08:09:23 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.find_all do |diction|
+      diction.split("").sort.join == word.split("").sort.join
+    end
+  end
+
+end
+
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+# => ["inlets"]
