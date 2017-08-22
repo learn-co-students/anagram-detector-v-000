892cf5ddaf8945681ef412c857b4c5c6d94058ce
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-08-22 18:17:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+end
+
+def match(array)
+
+  array.select {|n| n.split("").sort == @name.split("").sort}
+end
+
+
+end
