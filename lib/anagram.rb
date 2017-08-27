1a4ff3205ffcafbcf51862b6f5844a9fd29c00f8
Chris Rudolph <chrisjames.r@gmail.com>
2017-08-27 15:28:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+   def match(array)
+      array.select do |anagram| # Select returns "anagram" array for block == true
+        anagram if anagram.split("").sort == @word.split("").sort
+       # ^ return "anagram" for alphabetized anagram letter array comparison to test word
+     end
+
+   end
+
+end # <-- End of Class
