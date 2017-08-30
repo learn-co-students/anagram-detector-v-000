94f29e9c8c84619c4a927ad60e5e3931a8616d2e
Jameson Bass <jamesonbass@gmail.com>
2017-08-30 00:33:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(phrase)
+    phrase.find_all { |w| w.split(//).to_a.sort == @word.split(//).to_a.sort }
+  end
+
+end
