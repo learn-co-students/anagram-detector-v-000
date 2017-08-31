2d32498e69916ff9dab510f9344de5b10a8bd150
Laura Webber <wwebby1@gmail.com>
2017-08-31 01:26:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(word)
+    @anagram = word
+  end
+
+  def match(possibles)#array of possible anagrams
+    possibles.select {|letters| letters.split("").sort == @anagram.split("").sort}
+    #I was modifying them in place before; this returns the letters in original word order
+  end
+end
