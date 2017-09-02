a884ec7842738619b24760c96a2360b957f76427
Yumekia Ward <yumekiaw@yahoo.com>
2017-09-02 16:28:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(anagrams)
+    anagrams.keep_if{|word| word.split("").sort == @words.split("").sort}
+  end
+
+end
