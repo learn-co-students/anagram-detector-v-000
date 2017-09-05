503cb99e456995d8f9ba2b1fea1a95af10dfd633
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-05 18:50:00 +0000

    Create #match method and returns anagram(s). Specs are gren.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    anagrams = []
+    words_array.each do |test_word|
+      if test_word.split("").sort == word.split("").sort
+        anagrams << test_word
+      else
+        anagrams
+      end
+    end
+    anagrams
+  end
+
+end
