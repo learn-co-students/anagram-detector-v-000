41795d4e8b38d59e8ddb309736ca0e5c7b324cdb
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-05 18:56:34 +0000

    Refactor #match method to use .select instead of .each

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,16 +8,8 @@ class Anagram
     @word = word
   end
 
-  def match(words_array)
-    anagrams = []
-    words_array.each do |test_word|
-      if test_word.split("").sort == word.split("").sort
-        anagrams << test_word
-      else
-        anagrams
-      end
-    end
-    anagrams
+  def match(words)
+    words.select { |w| w.split("").sort == word.split("").sort }
   end
 
 end
