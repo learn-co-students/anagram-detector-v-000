d3235d3a5ae4469e507ce5514b7037345498901c
Janusz Szubert <jszubert87@gmail.com>
2017-09-06 14:14:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |el| el.split("").sort === word.split("").sort }
+  end
+
+end
