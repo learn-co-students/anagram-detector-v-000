90bc68de24e7820b50f49aedb69d410d2b74a122
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-09 03:15:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(string)
+    updated_array = []
+
+    new_array = string.to_a
+    new_array.each do |word|
+
+      array1 = word.chars.to_a
+      array2 = @word.chars.to_a
+
+      if array1.sort == array2.sort
+        updated_array << word
+      end
+    end
+    updated_array
+  end
+
+end
