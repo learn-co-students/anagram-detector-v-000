a42c2ac79d802aca62bb05ed6a345d0192966574
Hunter Kleindl <hkleindl@gmail.com>
2017-09-09 19:11:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |i|
+      i.split("").sort.join == word.split("").sort.join
+    end
+  end
+
+end
