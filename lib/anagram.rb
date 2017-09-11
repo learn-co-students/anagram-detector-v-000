731698442a81c404b05e6832c7b7f8e6bad8b6da
Robin Pahlman <robin.pahlman@gmail.com>
2017-09-11 15:05:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    result = []
+    split_word = @word.split("")
+    list.each do |item|
+      result << item if item.split("").sort == split_word.sort
+    end
+    result
+  end
+
+end
+
+#split_word = @word.split("")
+#split_list = list.map {|word| word.split("")}
+#count = 0
+#match = nil
+#  while count < split_list.length
+#    if split_list[count].sort != split_word.sort
+#      count += 1
+#    elsif split_list[count].sort == split_word.sort
+#      match = split_list[count].join
+#    end
+#  end
+#  match
