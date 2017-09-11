6b8006de0bc9eec3de155869e23e6bd035921813
Sofia Candiani <sofia.candiani123@gmail.com>
2017-09-11 20:14:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  #creates reader/writer for name
+  attr_accessor :name
+  def initialize(word)
+    #initializes word for name
+    @name = word
+  end
+  def match(matches)
+    #accepts an argument matches
+    #selects those that match
+    #splits array and sorts
+      matches.select {|m| m.split("").sort == @name.split("").sort}
+  end
+end
