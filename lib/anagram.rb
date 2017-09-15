0f58f895906f98bbb1235092cea5fcc9996016be
Stephen Decker <sdeckerm@gmail.com>
2017-09-15 15:05:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(words)
+    words.select {|w| self.words.split("").sort == w.split("").sort}
+  end
+
+end
+
+# if words.split.sort == w.split.sort
+#   words
+# else
+#   []
+# end
+#
+# if any?
+#
+#   words.join.split("").sort
+#
+#   words.join.split.sort
+#
+#   w.split("").sort
+#
+#   def match(words)
+#     words.map do |w|
+#       if self.words.split("").sort == w.split("").sort
+#     end
+#   end
