47ca302112c86f120494035d0b6b522e6a3d13dc
Ariana Berger <ariana.berger@gmail.com>
2017-09-17 22:10:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(word)
+    @anagram = word
+  end
+
+  def match(array)
+    new = []
+    array.each do |i|
+      if i.split("").sort == @anagram.split("").sort
+        then new << i
+      end
+    end
+    new
+  end
+
+end
