84dc4aa80832155cde5e046c2f484c77d58a256d
Sergio Correa <correa.sergio@gmail.com>
2017-09-17 18:16:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,3 +14,10 @@ class Anagram
     anagrams
   end
 end
+
+#### ALTERNATIVE ####
+#  1.# select method iterates and returns array of elements that satisfy block
+#  2.no need for anagrams variable(Flag or Switch) to maintain state
+# def match(array)
+#     array.select {|x| x.split("").sort == @name.split("").sort}
+# end
