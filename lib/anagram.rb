92a2692728577a196ec7486c4912a6923590b13c
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-21 21:35:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :check_words
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(check_words)
+    check_words.each do |
+  end
+end
