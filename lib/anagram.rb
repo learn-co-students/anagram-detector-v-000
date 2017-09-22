be903edf66a4287d46afba68a323b23af1c5f706
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-22 19:31:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,6 @@
 # Your code goes here!
-require 'pry'
-
 class Anagram
-  attr_accessor :word, :check_words
+  attr_accessor :word
 
   def initialize(word)
     @word = word
@@ -13,15 +11,11 @@ class Anagram
     word_split = @word.split("")
     check_words.each do |index|
       check_words_letters = index.split("")
-      @intersection = check_words_letters & word_split #gives me all the matches in a new array (not picking up duplicates)
-      if @intersection.length == index.length #comparing matches length to index length
+      intersection = (check_words_letters & word_split).flat_map { |n| [n]*[check_words_letters.count(n), word_split.count(n)].min }
+      if intersection.length == index.length
         result << index
       end
     end
     result
   end
-
 end
-
-listen = Anagram.new("allergy")
-listen.match(%w(gallery regally largely))
