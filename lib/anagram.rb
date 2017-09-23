ced606106b2f500eb3252175225beaf76155cc0c
ff18qd <ff18qd@gmail.com>
2017-09-23 22:00:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :str
+
+  def initialize(word)
+    @str=word
+  end 
+
+  def match(anagrams)
+    result=[]
+    anagrams.each do |item|
+      if item.split("").sort == @str.split("").sort
+        result << item
+      end 
+    end 
+    result
+  end 
+  
+end 
