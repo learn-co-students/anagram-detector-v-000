34e3277b0a7387bbfea7b23248a62778627bb889
kelly <github email address>
2017-09-24 03:52:21 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+
+ attr_accessor :anagram
+ 
+  def initialize(word)
+  	@@anagram = word
+  end
+
+  def match(word)
+  	matches = []
+    word.each do |test|
+        split_test = test.split("")
+    	split_anagram = @@anagram.split("")
+    	#shared = split_test & split_anagram
+    	if split_test.sort.eql?(split_anagram.sort)
+    		matches << test
+    	else
+    	end
+    end
+    matches
+  end
+
+end
