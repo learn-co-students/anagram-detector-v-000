ba4332918c294f1e3333f0e45ce3e461be550df5
dsbondin <dsbondin@gmail.com>
2017-09-26 23:08:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def match(array)
+      matches = []
+      array.each do |w|
+        if word.upcase.split("").sort == w.upcase.split("").sort
+          matches << w
+        end
+      end
+      matches
+    end
+
+end
