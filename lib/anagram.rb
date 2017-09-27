fff65aceebc70ccf54e21819fcb158dcc462c9e6
alpbourne <alpbourne@gmail.com>
2017-09-27 21:08:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    array_of_words.select do |possible_anagrams|
+      possible_anagrams.split("").sort == @word.split("").sort
+    end
+  end
+
+end
