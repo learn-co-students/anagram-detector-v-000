09cc020d7052e0eafa4a13e568f863cc1d35ac74
mevanstron <michael@michaelgregoryevans.com>
2017-09-28 17:08:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @name.split("").sort
+    end
+  end
+
+end
