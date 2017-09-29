560d0fffb821aa5f348810a7a0f93e2a02b0e0de
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-29 05:12:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
     @word = word
   end
 
-  def match(anagram_array)
+  def match(array)
     split_words = []
     final_array = []
     sorted_word = word.split("").sort
@@ -18,13 +18,11 @@ class Anagram
     end
     split_words.each_with_index do |split_word, i|
       if sorted_word == split_word
-        final_array << anagram_array[i]
+        final_array << array[i]
       end
     end
     final_array
   end
+  #
 
 end
-
-# usr/local/rvm/gems/ruby-2.3.1/bin/rspec:23:in `load': /home/nmhalloran-131086/code/labs/anagram-detector-v-000/lib/anagram.rb:14: syntax error, unexpected '\n', expecti
-# ng '=' (SyntaxError)
