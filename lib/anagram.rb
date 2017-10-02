d6db5d5773b68ca7cba1971231d66341eaed0c73
Cristina Collazos <clcgalex@gmail.com>
2017-10-02 23:56:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    @word = @word.split("")
+    anagrams.find_all do |anagram|
+      anagram = anagram.split("")
+      anagram.sort == @word.sort
+    end
+  end
+end
