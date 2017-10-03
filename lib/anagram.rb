72ac5633aa015cf8124c28e15e3051ff32da933c
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-03 01:49:03 +0000

    refactor code with .find_all

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,22 @@
 # Your code goes here!
+# class Anagram
+#   attr_accessor :word
+#
+#   def initialize(word)
+#     @word = word
+#   end
+#
+#   def match(array)
+#     arr = []
+#     array.each do |a_word|
+#       if a_word.split("").sort == @word.split("").sort
+#         arr << a_word
+#     end
+#   end
+#   arr
+# end
+# end
+
 class Anagram
   attr_accessor :word
 
@@ -7,12 +25,6 @@ class Anagram
   end
 
   def match(array)
-    arr = []
-    array.each do |a_word|
-      if a_word.split("").sort == @word.split("").sort
-        arr << a_word
-    end
+    array.find_all {|str| str.split("").sort == @word.split("").sort}
   end
-  arr
-end
 end
