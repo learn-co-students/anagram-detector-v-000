3a2c8814472e43982664637d70c8a572d590bcd7
Lara Sanders <larasanders@gmail.com>
2017-10-04 00:44:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :detector
+
+  def initialize(detector)
+    @detector = detector
+  end
+detector = Anagram.new(detector)
+
+  def match(words)
+      detector.chars.sort
+      words.select{ |word| word.chars.sort == detector.chars.sort}.map{ |word| word}
+end
+end
