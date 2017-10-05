48e4d2b06c606f829000c5b7ae92d173d671e5ec
Jerry Boyce <jokerdelta@yahoo.com>
2017-10-05 03:30:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
 
   def initialize(word)
     @word = word #set instance variable for match word
-    @anagram_array = []
+    @anagram_array = [] #set instance array to empty array
     self.match #call match method
   end
 
@@ -15,17 +15,13 @@ class Anagram
     #iterate over "array", compare each element
     #of array to "word" passed in on initilization
     array.each do |item|
-      array_word_sort = item.split("").sort
-      word_sort = @word.split("").sort
-      if array_word_sort == word_sort
-        @anagram_array << item
+      array_word_sort = item.split("").sort #split array word into an array of letters and sort
+      word_sort = @word.split("").sort #split word into an array of letters and sort
+      if array_word_sort == word_sort #compare each array of letters, if they're equal
+        @anagram_array << item # push array word to anagram array
       end
-      #split array word into an array of letters and sort
-      #split word into an array of letters and sort
-      #compare each array of letters
     end
-    @anagram_array
+    @anagram_array # return array of matching anagrams
   end
-
 
 end
