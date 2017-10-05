6c56eeec221d4e3950ae77f6b1777171dbcb432c
Jerry Boyce <jokerdelta@yahoo.com>
2017-10-05 03:25:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  @word = "" #initialize empty string
+
+
+  def initialize(word)
+    @word = word #set instance variable for match word
+    @anagram_array = []
+    self.match #call match method
+  end
+
+  def match(array = %w(default))
+    #iterate over "array", compare each element
+    #of array to "word" passed in on initilization
+    array.each do |item|
+      array_word_sort = item.split("").sort
+      word_sort = @word.split("").sort
+      if array_word_sort == word_sort
+        @anagram_array << item
+      end
+      #split array word into an array of letters and sort
+      #split word into an array of letters and sort
+      #compare each array of letters
+    end
+    @anagram_array
+  end
+
+
+end
