cc6526e328d3fbfc19003fd05bd296a7bff1430c
zolkocarbon <eathena@hotmail.com>
2017-10-09 16:12:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = @word.split(//)
+    Anagram_array = []
+    array.collect do |array_word|
+      array_word.split(//)
+
+
+
+    end
+  end
+
+end
