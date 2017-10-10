1ecb5fecaa05c08c38384fd6d6f4f0de3cc8a6b0
zolkocarbon <eathena@hotmail.com>
2017-10-10 12:53:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Anagram
 
   attr_accessor :word
@@ -8,13 +9,27 @@ class Anagram
 
   def match(array)
     word_array = @word.split(//)
-    Anagram_array = []
-    array.collect do |array_word|
-      array_word.split(//)
-
-
-#https://apidock.com/ruby/Array/include%3F
-    end
-  end
-
+    anagram_array = []
+    array.each do |array_word|
+      switch = 0 #initiate switch, re-set for each array_word
+      array_letter = array_word.split(//)
+        word_array.each do |letter|
+          if switch == 1
+            next #go to next array_word and get out of .each loop
+          end
+          if !array_letter.include?(letter)
+            #test each letter of class against array broken down into letter
+            switch = 1
+          end
+        end #for array_letter.each
+        if switch == 0 && word_array.length == array_letter.length
+          #has to have second test because sometimes ab class with abc would match
+          anagram_array << array_word
+        end
+      end #for array.collect
+      anagram_array
+    end #for method closing
 end
+
+#https://stackoverflow.com/questions/5286861/how-to-break-from-nested-loops-in-ruby
+# RESOURCE for "next" in ruby
