13c81d76ab7d71eb5850af25aa73be18dc1bb0bc
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 00:23:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.select {|anagram| anagram.split("").sort.join == @word.split("").sort.join}
+  end
+
+end
