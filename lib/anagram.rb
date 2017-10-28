921ee412f686569cd9e53511887442b4fc0dbce8
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-28 04:56:18 +0000

    removed comments of unusable code

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,4 @@
-# Your code goes here!
-# class Anagram
-#   attr_accessor :word
-#
-#   def initialize(word)
-#     @word = word
-#   end
-#
-#   def match(array)
-#     arr = []
-#     array.each do |a_word|
-#       if a_word.split("").sort == @word.split("").sort
-#         arr << a_word
-#     end
-#   end
-#   arr
-# end
-# end
+require 'pry'
 
 class Anagram
   attr_accessor :word
@@ -26,5 +9,6 @@ class Anagram
 
   def match(array)
     array.find_all {|str| str.split("").sort == @word.split("").sort}
+    binding.pry
   end
 end
