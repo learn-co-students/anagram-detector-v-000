c9fabaaece9eae3216151f981bbe691ec5f80b9e
Yosa Yon <yosa.yon@gmail.com>
2017-10-31 18:40:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def matches
+    @matches
+  end
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+  anagrams.find_all do |anagram|
+    anagram.split("").sort == @word.split("").sort
+  end
+end
+
+end
