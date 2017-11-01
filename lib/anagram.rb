d77fa5261377d2923a6887a4d5cef5bfd6182a6d
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-11-01 18:16:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matches = []
+    array.each do |el|
+      if @word.length == el.length
+        el_array = el.split("")
+        char_array = []
+        @word.split("").each do |char|
+          #take out the matching letters in the word in the word array
+          char_array << el_array.slice!(el_array.index(char)) if el_array.include?(char)
+        end
+        #a match if the el_array is now empty OR if char_array == @word
+        matches << el if el_array.length == 0
+      end
+    end
+    #each word ends here
+    matches
+  end
+end
