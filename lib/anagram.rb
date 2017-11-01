748becb2086d90feabd34c30d200a4639749e6f0
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 14:15:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word_to_match
+
+  def initialize(word)
+    @word_to_match = word
+  end
+
+  def match(array_of_words)
+    result = []
+    array_of_words.each do |word|
+      if word.split("").sort == word_to_match.split("").sort
+        result << word
+      end
+    end
+    result
+  end
+
+
+
+
+
+
+end
