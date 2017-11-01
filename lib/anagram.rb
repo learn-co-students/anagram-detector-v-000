6ed92c300c66e3961a9fd799e18f11c2c6f6a877
mmb16 <mmb583@nyu.edu>
2017-11-01 20:32:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :anagram, :sorted_gram
+
+  def initialize(anagram)
+    @anagram = anagram
+    sorted_gram
+  end
+
+  def sorted_gram
+    @sorted_gram = @anagram.split("").sort
+  end
+
+  def match(array)
+    matching = []
+    array.each do |word| gram = word.split("")
+      #binding.pry
+      if gram.sort == @sorted_gram
+        matching << word
+      end
+    end
+    matching
+  end
+
+end
