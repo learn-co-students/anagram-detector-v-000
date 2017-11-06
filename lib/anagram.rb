9eb96fab985db43f6a585a447609d8562e9684bd
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-06 00:57:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+  def initialize(new_word)
+    @word = new_word
+  end
+
+  def match(array)
+    array.select do |word_comp|
+      self.word.split("").sort == word_comp.split("").sort
+    end
+  end
+end
