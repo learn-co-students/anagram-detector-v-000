fe683e7861a0cadef3243c2c64c9eb7a21678caa
Parker Catalano <catalano.work@gmail.com>
2017-11-07 22:31:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    # Sort Letter Length
+    # Sort letters
+    # Are letters the same?
+    array.select do |letters|
+      letters.split("").sort == @word.split("").sort
+    end
+  end
+
+end
