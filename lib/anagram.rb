4bd2521647058fbc8386f11e30973a6bf0cec52a
Felix Lemerle <felix@lemerle.fr>
2017-11-10 23:10:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    list.map { |possible_anagram| possible_anagram if possible_anagram.split("").sort == word.split("").sort }.compact
+  end
+end
