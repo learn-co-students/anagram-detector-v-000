f1c32001aed1c819213243b5c787cef859a0389a
Morgan Fudge <mfudge92@gmail.com>
2017-11-11 19:46:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word.split("").sort
+  end
+
+  def match(arr)
+    match = Array.new
+    arr.each do |v|
+      search = v.split("").sort
+      if search == @word
+        match <<  v
+      end
+    end
+    match
+  end
+
+end
