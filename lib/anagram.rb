93ac0b590fef9f466db07e1053068109949112f3
Blakeley Bond <burrick2003@yahoo.com>
2017-11-11 03:01:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word_input, :match_input
+
+  def initialize(input)
+    @word_input = input
+  end
+
+  def match(input)
+    @match_input = input
+    @match_input.select do |element|
+      @word_input.split("").sort == element.split("").sort
+    end
+  end
+end
