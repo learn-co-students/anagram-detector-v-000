356ebe0975d561d8bcd6615ab093923cde0d9493
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-12 17:17:48 -0500

    Speclemented Anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(potential_anagrams)
+    word_as_sorted_charray = @word.split("").sort
+
+    anagrams = potential_anagrams.select do |potential_anagram|
+      word_as_sorted_charray == potential_anagram.split("").sort
+    end
+
+    anagrams
+  end
+end
