f0a815931373c3fe54f3c400efb3bc0013656810
amckean12 <amckean@me.com>
2017-11-14 18:25:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def anagram?(word)
+    word.chars.sort == @anagram.chars.sort
+  end
+
+  def match(words)
+    words.select do |word|
+      anagram?(word)
+    end
+  end
+end
