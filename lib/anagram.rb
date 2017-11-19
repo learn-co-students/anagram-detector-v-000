9e99db3534d8113ce78489e431184193617303de
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-19 22:09:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    matches = []
+    list.each do |word|
+    if word.split("").sort == @word.split("").sort
+      matches << word
+    end
+  end
+  matches
+  end
+end
