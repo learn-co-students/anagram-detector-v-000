aaae1f27d14e09cf4f334ef328193ae55c1d8c32
Laura Trager <laura.trager@gmail.com>
2017-11-21 01:36:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    matches = []
+    letter_array = word_array.collect{|word| word.split("")}
+      # => [["a"], ["a", "b", "c"], ["a", "d", "b"], ["a", "d", "d"], ["a", "b", "c"], ["a", "c"], ["d", "b"]]
+    split_word = @word.split("")
+      # => ["a", "b"]
+    letter_array.find_all do |letters|
+      if letters.sort == split_word.sort # => false, false, false, false, true, false, false
+        matches << letters.join
+      end
+    end
+    matches
+  end
+
+end
