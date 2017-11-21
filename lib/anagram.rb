c6077e74547be2211d51193cc8c90fb5d8ea167c
Charity <Youngblood.charity@gmail.com>
2017-11-21 12:49:29 -0800

    added comments on how to approach project

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+# Write a program that, given a word and a list of possible anagrams, selects the correct one(s).
+# Your class, Anagram should take a word on initialization
+# instances should respond to a match method that takes an array of possible anagrams. It should return all matches in an array. If no matches exist, it should return an empty array.
+
+# create Anagram class
+class Anagram
+# attr_accessor - not sure which one yet, but should be included in class
+# initialize with word 
+  
+# create instance methods 
+# method should receive and array of possible anagrams
+# method should match anagrams with the value passed through the word argument 
+# return value should be an array of all matches
+# if no matches exist, return value is empty array
+  
+  
+  
+  
+  
+  
+  
+  
+end
