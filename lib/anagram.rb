048611d9d330936f5dca89e3e8a347a8aedcce74
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-22 00:36:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possibilities)
+    split_word = self.word.split("").sort
+    possibilities.find_all do |possibility|
+      split_possibility = possibility.split("").sort
+      split_possibility == split_word
+    end
+  end
+end
