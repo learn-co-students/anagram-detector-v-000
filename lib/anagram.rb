1762d90824fd179dcf161130640a4b8b48f1ad1c
charityyoungblood <youngblood.charity@gmail.com>
2017-11-22 08:39:44 -0800

    refactored code - added comparison operator, shortened code to one line

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,18 +16,14 @@ class Anagram
     end 
 # create instance methods 
     def match(word_list) # word_list is an array
-      
-        new_anagram = @anagram_list.split("").sort # takes the word argument that is passed through the method, splits it into individual characters, then sorts into alphabetical order  
-      
-        new_word_list = word_list.map {|w| w.split("").sort} # this takes the array that the test is passing through the match method and splits into individual characters then sorts into alphabetical order
-        if new_word_list.include?(new_anagram) # the if statement asks if the value for new_anagram is included in the new_word_list variable. If true, it will search the array for a match to the new_anagram variable
-          @select_word = new_word_list.select {|match| match == new_anagram} 
-          @select_word.map {|n| n.join} # this joins the array back together 
-        else
-        new_word_list.clear # else it will return an empty array
+    
+    word_list.keep_if { |str| @anagram_list.split("").sort == str.split("").sort}
+        
+      # @anagram_list.split("").sort # takes the word argument that is passed through the method, splits it into individual characters, then sorts into alphabetical order
+      # str.split("").sort # this takes the array that the test is passing through the match method and splits into individual characters then sorts into alphabetical order
+#      == compares the two values
         end
-        
-        
+    
       # what does an anagram match look like? - try matching the set of letters passed through the method
       # all anagrams are the same when sorted
 #       - split the words in multiple arrays 
@@ -35,7 +31,7 @@ class Anagram
 #       - check for matches 
       
      # this method should go through word_list array and detect if there are any anagram matches 
-    end 
+  
 # method should receive and array of possible anagrams
 # method should match anagrams with the value passed through the word argument 
 # return value should be an array of all matches
