3e5fe3baa63a5dfbad81d51e1bef213567aabe1e
Jessica Alonso <alonso.jessicam@gmail.com>
2017-12-07 15:30:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.find_all {|a| a.split("").sort == @word.split("").sort}
+  end
+
+end
