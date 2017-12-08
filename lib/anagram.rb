42790ebb8837f01d832a0b284e022b39b9526b8b
Nour Kodeih <nour.kodeih@gmail.com>
2017-12-08 02:25:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    sorted_word = @word.split("").sort.join("")
+    array.select do |anagram|
+      anagram.split("").sort.join("") == sorted_word
+    end
+  end
+end
