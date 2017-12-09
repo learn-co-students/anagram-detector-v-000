7b6df5259131fa84e25bba9483a760f8cbc0a63e
Richard Schaubeck <richard.schaubeck@gmail.com>
2017-12-09 00:52:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    sorted_word = @word.split("").sort
+    arr.select do |word|
+      word.split("").sort == sorted_word 
+    end
+  end
+
+end
