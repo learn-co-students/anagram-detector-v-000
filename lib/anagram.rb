fe1a1db9e62e45ad00d869a3f59678b965f49182
Connor Gallopo <connor.gallopo@me.com>
2017-12-11 14:18:50 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require "pry"
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	# def sort_each_word(obj)
+	# 	if obj.class == Array
+	# 		new_array = []
+	# 		obj.each { |i| new_array << i.chars.sort(&:casecmp).join}
+	# 		new_array
+	# 	elsif obj.class == String
+	# 		new_string = obj.chars.sort(&:casecmp).join
+	# 		new_string
+	# 	end
+	# end
+
+	def match(array)
+		array.select {|x| x.split("").sort == @word.split("").sort}
+	end
+end
