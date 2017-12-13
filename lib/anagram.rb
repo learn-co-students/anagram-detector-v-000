3c54870659acdeeb4049aea10159dd85b72dfb8e
Brittany Morris <brittmmendez@gmail.com>
2017-12-13 18:06:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,8 @@ class Anagram
   end
 
   def match(array)
+    #instead of LOC:10-17, could have used->  array.select {|x| x.split("").sort == @name.split("").sort}
     return_array=[]
-#instead of LOC:12-17, could have used->  array.select {|x| x.split("").sort == @name.split("").sort}
     array.each do |anagram_word|
        if anagram_word.split("").sort==@word.split("").sort
          return_array<<anagram_word
