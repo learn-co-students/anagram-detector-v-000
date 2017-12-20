2e53e078715273b53fb545f383d77b1ac24ca2d7
Chrissa Constantine <chrissa1@mac.com>
2017-12-20 19:35:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+#have to compare two things - use sort, use a higher level iterator (not each) - select. Iterates over the characters of the words.
+  def match(words)
+    words.select { |word| word.chars.sort == @word.chars.sort}
+  end
+
+end
