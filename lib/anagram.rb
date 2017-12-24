f4138b7813af55033bed9545568ecb666613159d
Eddie Melo <heriberto@melo.nyc>
2017-12-24 20:32:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select {|reversed_word| reversed_word.chars.sort == word.chars.sort}
+  end
+end
