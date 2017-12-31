1974cbb68a6cbb95dfe8220f31fcf91d6e58c5ca
TravisA <github email address>
2017-12-30 21:10:54 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  attr_reader :anagram_word
+  def initialize(string)
+  	@anagram_word = string
+  end
+
+  def match(array)
+  	word_to_match = @anagram_word.split("").sort
+  	array.select do |word|
+  	  new_word = word.split("").sort
+  	  if new_word == word_to_match
+  	  	word
+  	  else false
+  	  end
+  	end
+  end
+end
