884802c43b62e5a9d5293c93ebb373140746666c
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-06 17:21:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word_to_match
+
+  def initialize(word)
+    @word_to_match = word
+  end
+
+  def match(arr_of_words)
+    arr_of_words.find_all do |word|
+      word.split("").sort == @word_to_match.split("").sort
+    end
+  end
+
+end
