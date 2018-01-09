0999307be4254df01670fbc6d1396753120ff561
Brad Hambrice <hambrice@uab.edu>
2018-01-09 00:57:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word 
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    new_array = []
+    array.collect do |option|
+      new_array << option if word.split("").sort == option.split("").sort 
+    end
+    new_array.delete_if do |options|
+      options == nil 
+    end
+end
+end
