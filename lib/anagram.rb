24c21fec5a4a0f2a44b54cfb121300bdd9eb84f7
Jordan Kline <jordan.kline@gmail.com>
2018-01-09 16:33:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.collect { |i| i if i.chars.sort == @word.chars.sort }.compact
+  end
+
+end
