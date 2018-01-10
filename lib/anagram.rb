e8fb7077c226a8c9c7bb3d57005698a97631e380
An Nuyen <annuyen@gmail.com>
2018-01-10 17:27:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :term
+
+  def initialize(term)
+    @term = term
+  end
+
+  def match(anagram)
+    anagram.select {|i| i.chars.sort == term.chars.sort}
+  end
+end
