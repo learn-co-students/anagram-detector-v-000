bb4581291a6c33df04ebeabacb362a35df313947
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-14 16:39:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :anagrams
+
+  def initialize(anagrams)
+    @anagrams = anagrams
+  end
+
+  def match(words)
+    words.select { |w| w.chars.sort == anagrams.chars.sort }
+  end
+
+end
