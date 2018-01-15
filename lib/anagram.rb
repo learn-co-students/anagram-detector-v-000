9e274305720aa239ec945797f09761ab9a3a3842
kanndide <kanndide@gmail.com>
2018-01-15 20:12:14 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    
+    attr_accessor :name
+    
+    
+    def initialize(name)
+        @name = name
+    end
+    
+    def match(string)
+               
+        string.select {|x| x.chars.sort == @name.chars.sort} 
+        
+    end
+    
+end
+
