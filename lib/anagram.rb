3fad5841e0c4095a448575556144cd62b1639db1
EPIC448 <samrey2018@gmail.com>
2018-01-15 21:13:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+
+    def initialize(word)
+            @word = word
+    end
+
+    def match(array)
+      array.select{|x| x.split('').sort == @word.split('').sort} # x can be anthing
+    #  .sort go over each character of each word to find a match ot not
+     #  . sort put allalphibetas. s
+  #abt = tab
+
+    end
+
+
+
+end
