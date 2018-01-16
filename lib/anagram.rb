7d8afca5866278f7a485aaedc968785550590540
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-16 16:24:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,11 @@
 # Your code goes here!
 require 'pry'
 =begin
+First solution attempt.
     we need to build a regexp(a string) that consists of an word repeated word.length times. [word][word][word][word]
     save that string into a variable
     use grep to return an array of matches to the regexp(string)
-=end
 
-
-class Anagram
-
-  attr_accessor :name
-
-  def initialize(word)
-    @name = word
-  end
-
-  def match(array)
-=begin
     #word_length = @name.length
     repeating_array = []
     new_array = []
@@ -29,6 +18,22 @@ class Anagram
     regexpstring =`/#{repeating_array.join}\b/`
     array.grep(regexpstring)
 =end
+
+#NOTES>>>> I spent way too long on this lab trying to make my pseduo code work.
+#I should have stepped back and reaccessed the code. There was surely a more simple way.
+#before beginning, consider which BUILT IN OBJECTS CAN HELP US ACHIEVE THE GOAL.
+#SORT is good. building a weird REGEXP for each implementation is too much work!!
+
+
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
 
   #iterate with collect/map and use sort
   #if letters equal, return the word
