7281986ce2627a7eb9d82c33442825415d15d48c
Matthew Crooks <mfcrooks@gmail.com>
2018-01-17 08:30:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |w| w.split("").sort == @word.split("").sort}  #Break up words and sort by order, if it matches than it's an anagram!
+  end
+end
