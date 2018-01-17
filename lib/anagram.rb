f143925b5e615d3fe44e72a99153439c7679a0cf
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-17 21:16:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize
+    @word  = word
+  end
+
+  def match
+    string = "hello world zombies pants dipper"
+    split_word = word.split("").sort
+    string.split(" ").collect{|w| w.sort =  word.split("").sort}
+  end
+
+end 
