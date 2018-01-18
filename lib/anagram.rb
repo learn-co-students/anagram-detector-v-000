ea04419bea0c3484d689b567e297fd5ed710b1bc
Ingrid Wong <yw340@georgetown.edu>
2018-01-18 02:50:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    #for each word in possible_anagrams
+      #
+    #return an array of anagrams
+
+    possible_anagrams.select do |possible|
+      possible.sort == word.sort
+    end
+
+  end
+end
