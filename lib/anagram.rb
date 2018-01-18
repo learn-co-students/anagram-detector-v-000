ec9f0d67db065b9396f7eeb4309f8b88b43932ae
Ingrid Wong <yw340@georgetown.edu>
2018-01-18 02:52:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,10 +6,6 @@ class Anagram
   end
 
   def match(possible_anagrams)
-    #for each word in possible_anagrams
-      #
-    #return an array of anagrams
-
     possible_anagrams.select do |possible|
       possible.split("").sort == word.split("").sort
     end
