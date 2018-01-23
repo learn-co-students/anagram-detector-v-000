bb0796433c7bcbe63a7bd8d0ffea938aad29a21d
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-23 22:48:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+   match =  array.keep_if {|word| word.split("").sort == @word.split("").sort}
+    match 
+    
+
+  end
+  
+end 
