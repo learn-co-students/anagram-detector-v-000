f4318e55d2a8c5ae1cebe4c4af9a70be05b2ebf4
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-03 14:05:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    matches = []
+    word_test = word.split(//)
+    word_test = word_test.sort!
+    word_array.each do |w|
+      letters = w.split(//)
+      letters = letters.sort!
+      if letters == word_test
+        matches << w
+      end
+    end
+    matches
+  end
+
+end
