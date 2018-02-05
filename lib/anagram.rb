03a20505a872765143418679c42f31fc8246c94d
Kim Gonzales <kimgo589@gmail.com>
2018-02-05 23:12:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(some_words)
+    some_words.find_all do |maybe_match|
+      word.split("").sort.join == maybe_match.split("").sort.join
+    end
+  end
+
+
+end
