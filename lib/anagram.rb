6754dff2c886ebb33dbc9c73a10b1d2e3c8faca3
Kriti Rai <kriti.rai@gmail.com>
2018-02-05 22:14:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(some_words)
+    some_words.select {|each_word|each_word.split("").sort == @word.split("").sort}
+  end
+
+ end
