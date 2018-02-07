9f68382008520e2b6a7bbf1a1d891430579640a9
Chaim Reisman <crreisman@gmail.com>
2018-02-07 14:29:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(word1)
+    
+    word1.select do |w|
+      #binding.pry
+      w.split("").sort == @word.split("").sort
+    end 
+  end 
+end 
