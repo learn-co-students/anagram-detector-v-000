b73d8212c9383a31ec33a4b32f635224c2c956a2
Brent  Ransom <sadumedusa@gmail.com>
2018-02-07 21:40:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word)
+    word.select do |letter|
+      (@word.split("").sort) == (letter.split("").sort)
+  end
+end
+end
