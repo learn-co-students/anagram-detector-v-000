72420ec80af016d0920a57c45e576b74f72d1372
Chris Henry <cdhenry@gmail.com>
2018-02-13 17:39:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word, :anagrams
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    matches = []
+    array = @word.split("")
+    sorted = array.sort
+    word_list.each do |match|
+      compare = match.split("")
+      sorted_match = compare.sort
+      if sorted == sorted_match
+        matches << match
+      end
+    end
+    matches
+  end
+end
