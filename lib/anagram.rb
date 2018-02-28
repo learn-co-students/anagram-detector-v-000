c75e93f6375059263dc2885e6a0bc9e5b07d8c20
Robert W <robwash@gmail.com>
2018-02-28 04:34:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,13 @@ class Anagram
   def match(word_list)
     # => Refactored
     # array.select {|x| x.split("").sort == @name.split("").sort}
-    
+
     result = []
     word_list.each do |word|
-      if word.length == @word.length
-        if word.split("").sort == @word.split("").sort
+      if word.split("").sort == @word.split("").sort
           result << word
         end
       end
-    end
     result
   end
 
