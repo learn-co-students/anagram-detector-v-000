a3fb06d4867f570f27e99dab316e2746e4190db0
Denise Pendleton <dpendl1@hotmail.com>
2018-03-04 20:25:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+    @word = word
+  end
+
+  def match(poss)
+    poss.select {|str| word.split('').sort == str.split('').sort}
+    # or
+    # poss.keep_if {|str| word.split('').sort == str.split('').sort}
+
+  end
+
+end
