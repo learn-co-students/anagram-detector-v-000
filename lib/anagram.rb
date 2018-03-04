40e87fa8203d2fe5ac3d2ae3b8c2756a4a7a29ad
Denise Pendleton <dpendl1@hotmail.com>
2018-03-04 21:19:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,25 @@
 # Your code goes here!
 
 class Anagram
-    attr_accessor :word
+    attr_accessor :word, :possibles
 
     def initialize(word)
     @word = word
   end
 
-  def match(poss)
-    poss.select {|str| word.split('').sort == str.split('').sort}
-    # or
-    # poss.keep_if {|str| word.split('').sort == str.split('').sort}
+  def match(possibles)
 
+    possibles.select {|str| word.split('').sort == str.split('').sort}
+    # or 1)
+    # possibles.keep_if {|str| word.split('').sort == str.split('').sort}
+    # or2)
+# this also works in repl.it but not according to these tests
+# def match
+#     @possibles = possibles
+#     new_word = @word.split('').sort
+#     new_arr = possibles.collect {|x| x.split(('')).sort}
+#     new_arr.select {|str| new_word == str}
+# end
   end
 
 end
