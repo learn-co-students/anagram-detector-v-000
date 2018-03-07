392cfccde9510159af1706272ff2b8644b7b95fa
Holden Altaffer <haltaffer@gmail.com>
2018-03-07 00:32:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :listen, :anagrams
+
+  def initialize(listen)
+    @listen  = listen
+  end
+
+  def match(anagrams)
+    @anagrams = anagrams
+    @anagrams.select {|anagram| anagram.split("").sort == @listen.split("").sort}
+  end
+
+
+end
