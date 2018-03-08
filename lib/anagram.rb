b40301998b02d69b2e5c127cc3227134e402fe60
Ayana Bando <ayanaflax@yahoo.com>
2018-03-08 17:11:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  @@all = []
+  attr_accessor :anagrams
+
+  def initialize(anagrams)
+    @anagrams = anagrams
+  end
+  def match(list)
+    list.select{|anagrams| anagrams.chars.sort == @anagrams.chars.sort}
+  end
+end
