e298f53fd7b982513659969a1e274286c6957d65
Aspen James <aspenjames@tqca.org>
2018-03-09 20:09:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require'pry'
+class Anagram
+  attr_accessor :input
+
+  def initialize(input)
+    @input = input.downcase
+  end
+
+  def match(arr)
+    word = @input.split("")
+    word.sort!
+    matches = []
+
+    arr.each_with_index do |w, i|
+      w = w.split("")
+      matches << i if w.sort == word
+    end
+
+    arr.find_all.with_index {|w, i| matches.include?(i)}
+  end
+
+end
