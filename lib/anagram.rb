7920763db8b6bb3d2cf4c002113cc52b131d010a
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-13 22:40:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,9 @@
-# Your code goes here!
+class Anagram
+  def initialize(subject)
+    @subject = subject
+  end
+
+  def match(words)
+    words.select{ |word| word.split('').sort == @subject.split('').sort }
+  end
+end
