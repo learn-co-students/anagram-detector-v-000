c060937c3d32cbbeffdec7a3e341ab582896d693
Olivia Hanson <olivia.joy.hanson@gmail.com>
2018-03-15 18:20:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = @word.split("")
+    array.select do |w|
+      w_array = w.split("")
+      w_array.sort == word_array.sort
+      
+    end
+  end
+
+end
