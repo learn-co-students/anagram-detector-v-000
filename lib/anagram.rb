dad239d5f7e37cdde013aaf5a02ec6e02e7c3bc8
Andrew Kairys <andrewkairys@gmail.com>
2018-03-19 23:14:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :anagram
+
+def initialize(word)
+  @anagram = word
+end
+
+def match(word)
+  word.select {|e| e.split("").sort == @anagram.split("").sort }
+end
+
+end
