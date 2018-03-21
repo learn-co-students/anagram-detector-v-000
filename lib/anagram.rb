90286a42aafbe306032f59e4dde0720265a298d7
Hiroki Kato <hkato@sas.upenn.edu>
2018-03-21 20:53:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,44 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+
+    anagram_match = []
+
+    possible_anagrams.each do |possible_anagram|
+      if possible_anagram.split("").sort == word.split("").sort
+        anagram_match << possible_anagram
+      end
+    end
+    anagram_match
+  end
+end
+
+
+
+# class Anagram
+#   attr_reader :word
+#
+#   def initialize(word)
+#     @word = word
+#   end
+#
+#   def match(possible_anagrams)
+#     anagram_match = []
+#     possible_anagrams.each do |possible_anagram|
+#       if possible_anagram.split("").sort == word.split("").sort
+#         anagram_match << possible_anagram
+#       else
+#         nil
+#       end
+#     end
+#     anagram_match
+#   end
+# end
