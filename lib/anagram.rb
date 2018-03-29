3d7bce8ef8a17b80ebf9f18f0cc2e5a96e6f2bbc
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-29 18:11:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :name
+
+def  initialize(word)
+  @name = word
+end
+
+def match(words)
+  new_array= []
+split_name = @name.split("")
+words.each do |word|
+  split_letters = word.split("")
+  if split_name.sort == split_letters.sort
+    new_array<<split_letters.join
+  end
+end
+new_array
+end
+
+
+end
