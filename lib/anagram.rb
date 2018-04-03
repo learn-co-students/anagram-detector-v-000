d997cec96d059dba8fbc34f92c12d1850ec03f4f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-03 19:35:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor  :word
+
+  def initialize (word)
+    @word = word
+  end
+
+  def match ( candidates )
+    word_sorted = @word.split('').sort
+    candidates.find_all { | candidate | candidate.split('').sort == word_sorted }
+  end
+
+end
