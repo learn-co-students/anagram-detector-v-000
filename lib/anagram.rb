ed4bc88bcffff474acfca1d85d8baa560a88b7eb
Kachel Wittig <kjwittig@gmail.com>
2018-04-03 20:43:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def match(word_arr)
+    word_arr.find_all { |m| alphabetizer(m) === alphabetizer(@word) }
+    # word_arr.find_all { |w| w.tr(@word, "") == "" }
+  end
+
+  def initialize(word)
+    @word = word
+  end
+
+  private
+
+  def alphabetizer(string)
+    string.chars.sort.join
+  end
+
+end
