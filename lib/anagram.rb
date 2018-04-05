c4bbd185f6931dac3391b01040360d34160df20b
Elias Ventura <eliasventura23@gmail.com>
2018-04-05 15:07:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,13 @@
+require 'pry'
 class Anagram
 
   attr_accessor :word
+
   def initialize(word)
     @word = word
   end
 
   def match(array)
-     array.select {|x| x.split("").sort == @word.split("").sort}
-   end
+    array.select {|x| x.split("").sort == word.split("").sort}
+  end
 end
