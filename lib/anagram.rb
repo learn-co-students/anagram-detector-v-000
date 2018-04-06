1b46b0a3307eec0a6202f63e71f1cec2dcea5f76
Jonathan Williams <jtw365@live.missouristate.edu>
2018-04-06 00:50:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.collect do |ana|
+      if word.split("").sort == ana.split("").sort
+        ana
+      end
+    end.compact
+
+
+    #binding.pry
+  end
+end
