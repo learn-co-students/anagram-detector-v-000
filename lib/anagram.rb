eebbd854589468a536329827b8683df78ffb1d9e
Chris Dundon <chris.dundon@gmail.com>
2018-04-07 02:15:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(arr_words)
+    obj_word = word
+    
+    obj_word = obj_word.split("").sort
+    
+    rtn = arr_words.select {|w| obj_word == w.split("").sort}
+    rtn
+  end
+end
