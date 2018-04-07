7635726ab5e59a4ba4c9bb6404cb5e415abd0910
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-04-07 03:10:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.map do |anagram|
+      possible_matches = anagram.split("")
+        x= possible_matches.sort
+        y = word.split.sort
+        if x == y
+          x
+      end
+    end
+  end
+  
+end
