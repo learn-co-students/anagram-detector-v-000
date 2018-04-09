665c5234ade4f35254e09cf9683e00b94ec5d9f4
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-09 23:05:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
+require 'pry'
+
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |str| 
+      word.split("").sort == str.split("").sort
+    end
+  end
+  
+  #array.keep_if { |str| word.split("").sort == str.split("").sort } 
+  
+end
