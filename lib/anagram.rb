df1240a950b334e35c349e0efe2c2475e1209591
Joanne Lee <vernapii@gmail.com>
2018-04-09 22:07:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+def initialize(word)
+  @word = word
+end
+
+def match(words_array)
+  words_array.each do |w|
+  if word[0..-1] == w[-1..0]
+    return w
+  end
+end
+end
