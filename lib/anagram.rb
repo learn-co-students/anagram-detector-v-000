13c1d6ebc7bee6f6f6c99f5a51cb326382943527
Madeline Stark <madeline.stark1@gmail.com>
2018-04-12 23:35:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(possibles)
+  grams = []
+  possibles.each do |string|
+    letters = string.split("")
+    wordy_letters = word.split("")
+    if letters.sort == wordy_letters.sort
+      gram = letters.join
+      grams << gram
+    end
+  end
+  grams
+end
+
+end
