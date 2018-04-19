fa10b7b7cd4fee0f74c58ca5609bd807a76b7654
Erica Murdock-Waters <ericahpa@gmail.com>
2018-04-19 03:50:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+# anagram.rb
+
+class Anagram
+  attr_accessor :ba, :match
+  @@all = []
+
+  def initialize(ba)
+    @ba = ba
+  end
+
+  def match(array_of_possible_anagrams)
+    array_of_possible_anagrams.select do |anagram|
+      if (anagram.split(//)).sort == (@ba.split(//)).sort
+        anagram
+      end
+    end
+  end
+
+end
