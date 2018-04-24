dab0501e6d3787da4ad3df58dfe6f9c4a58b9b7d
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-24 04:41:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(var)
+    matches = []
+    var.each do |ele|
+      matches << ele if objectify(@anagram) == objectify(ele)
+    end
+    matches
+  end
+
+  def objectify(word)
+    new_object = {}
+
+    word.split("").each do |ele|
+      new_object[ele] == nil ? new_object[ele] = 1 : new_object[ele] += 1
+    end
+    new_object
+  end
+end
