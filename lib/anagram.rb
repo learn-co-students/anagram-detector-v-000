4547097ff1fbec31f022284c8af9381aef88ddad
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-24 05:07:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,19 +7,6 @@ class Anagram
   end
 
   def match(var)
-    matches = []
-    var.each do |ele|
-      matches << ele if objectify(@anagram) == objectify(ele)
-    end
-    matches
-  end
-
-  def objectify(word)
-    new_object = {}
-
-    word.split("").each do |ele|
-      new_object[ele] == nil ? new_object[ele] = 1 : new_object[ele] += 1
-    end
-    new_object
+    var.select { |word| word.split("").sort == @anagram.split("").sort }
   end
 end
