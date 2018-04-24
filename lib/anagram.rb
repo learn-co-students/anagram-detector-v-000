24d932f3ed94cf7c66a4e37c4bd3ea1c5a15f60b
ERIC KELLY <eakelly220@gmail.com>
2018-04-24 23:51:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,14 @@
 class Anagram 
-  attr_accessor :word
-  
+  attr_accessor :word 
   def initialize(word)
     @word = word
   end 
   
 
   def match(strings) 
-  words = []
   letters =  @word.split("").collect {|letter| letter}
-    strings.select do |word| 
-       words << word  if letters.sort == word.split("").collect {|letter| letter}.sort
-      puts word
+    strings.select do |word|
+      word.split("").collect {|l| l}.sort == letters.sort
     end 
-    words
   end
 end 
