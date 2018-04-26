18135e77fcec9dff663b3d5395dc8ed33e55325c
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-26 20:28:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @word
+  end
+
+  def match(anagrams)
+    anagrams.select do|letters| 
+      @word.split("").sort == letters.split("").sort                # do they have the same letters anagrams[i] and @word
+    end
+  end
+
+end
