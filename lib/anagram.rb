7bfc80e5b89bb8e56ee86420ce6f54acc8229617
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-28 23:20:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(find_anagram_in_string)
+  find_anagram_in_string.find_all do |word|
+    if word.split("").sort == self.word.split("").sort
+      word
+    end
+  end
+end
+end
