d4d1ad92f3a1f51c1d5e02cd5ad3dcfc04fb954a
Kayvan Vakili <kvakili813@gmail.com>
2018-05-01 22:04:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+attr_accessor :anagrams
+
+def initialize(word)
+  @word = word
+end
+
+def match(words)
+  anagram = []
+  words.each do |scrambled|
+    if scrambled.split("").sort == @word.split("").sort
+      anagram << scrambled
+    end
+  end
+  anagram
+end
+
+
+end
