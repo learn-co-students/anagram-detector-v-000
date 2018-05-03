d7fe48fcccbb46ce206b71b5af73fdc789e2642d
Karl Amoguis <ktamoguis@gmail.com>
2018-05-03 09:24:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+#require 'pry'
+
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(matcharray)
+    newarray = []
+    matcharray.each do |arraywords|
+      if arraywords.split("").sort == @words.split("").sort
+        newarray << arraywords
+      else
+      end
+    end
+    newarray
+  end
+
+end
+
+#diaper = Anagram.new("diaper")
+
+#diaper.match(%w(hello world zombies pants dipper))
+
+#binding.pry
