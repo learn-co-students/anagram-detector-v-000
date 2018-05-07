7ea3106cb03f655909f3219bd2bcc1d990c2647b
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-07 19:15:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    new_array = []
+    array.each do |letters|
+      if letters.split("").sort == word.split("").sort
+      new_array << letters
+      end
+    end
+    new_array
+  end
+  #binding.pry
+end
