4f19d739d3c9dfebe244f5b7ea60bc7b4a56efb6
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-07 19:17:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,3 +18,19 @@ class Anagram
   end
   #binding.pry
 end
+
+=begin
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end
+
+end
+=end
