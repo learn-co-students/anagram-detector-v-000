b5ccef78d895ae1657f454326ec75fd62f0da9d2
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-08 19:26:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    sorted_word = word.split("").sort
+    array.select do |jumble|
+      jumble.split("").sort == sorted_word
+    end
+  end
+  
+end
