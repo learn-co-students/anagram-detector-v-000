926d453052f3ba07f71ebf2f56cc3f6d451351ad
Eric Fiorani <efiorani305@gmail.com>
2018-05-09 18:05:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :matches
+
+  def initialize(matches)
+    @matches = matches
+  end
+
+  def match(words)
+    words.select do |word|
+      anagram?(word)
+    end
+  end
+
+  def anagram?(word)
+    word.chars.sort == @matches.chars.sort
+  end
+end
+
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+# binding.pry
