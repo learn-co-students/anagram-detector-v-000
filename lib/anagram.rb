124bb7a805fd2993d1d16413660ec7812a1bb98c
Emily J Cain <emilycain@ymail.com>
2018-05-12 20:01:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram 
+  
+attr_accessor :word 
+
+def initialize(word)
+  @word = word 
+end 
+
+def match(array)
+  matching_array = []
+  word_letters = word.split("").sort 
+  array.each do |some_words|
+    if some_words.split("").sort == word_letters
+      matching_array << some_words  
+    end 
+  end 
+  matching_array
+end 
+  
+end 
