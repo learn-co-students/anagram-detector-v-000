503dacc76a6159bea233e464152e1bdf3e9ddc51
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-17 20:43:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
+require 'pry'
 # Your code goes here!
+# Build a class with an initialize method and use an attr_accessor
+class Anagram
+  attr_accessor :words
+
+# initialize method
+  def initialize(words)
+    @words = words
+  end
+
+  def match(array)
+    # iterate, split, compare ==, sort
+    array.find_all do |word|
+    word.split("").sort == @words.split("").sort
+    end
+  end
+
+end
