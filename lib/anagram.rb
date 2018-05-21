802d0f6399e6a9819d8111d5adf57136bf351170
Dmitri Mendis <2dmendis@gmail.com>
2018-05-21 19:40:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram 
+  
+  def initialize(word)
+    @anagram = word.split("")
+  end 
+  
+  #def match(possible)
+   # answer = []
+    #possible.each do |i|
+     # new = i.split("")
+  #     compare = new & @anagram 
+  #     if compare.length == @anagram.length 
+  #       answer << i  
+  #     end 
+  #   end 
+  #   answer
+    
+  # end 
+  
+  def match(possible)
+    answer = []
+    possible.each do |i|
+      new = i.split("")
+      compare = new.sort
+      compare_ana = @anagram.sort 
+      if compare == compare_ana
+        answer << i 
+      end 
+    end 
+    answer 
+  end 
+end 
