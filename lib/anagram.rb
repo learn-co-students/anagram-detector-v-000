23efac8ceeb4aaf5358276717d8f759f4476d783
Conor Hamilton <conhamil19@gmail.com>
2018-05-22 12:41:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram 
+  attr_reader :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |words|
+      @word.chars.sort.join == words.chars.sort.join
+    end
+  end 
+end
