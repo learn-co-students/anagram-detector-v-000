3dd50dbad77629ae3e93789f5a4a8029275d1cd7
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-22 01:11:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    sorted = self.word.chars.sort.join
+    words.select { |option| option.chars.sort.join == sorted }
+  end
+end
