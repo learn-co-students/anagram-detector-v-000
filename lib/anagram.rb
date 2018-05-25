6eb356a9a8b34376420843a8d33b05eab17c4b52
Laura Messa Travin <laura.travin@gmail.com>
2018-05-25 18:32:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+   attr_accessor :words
+
+   def initialize(w)
+     @words = w
+   end
+
+  def match(collection)
+    all = []
+       collection.each { |chr|
+
+         if @words.chars.sort.join == chr.chars.sort.join
+           all << chr
+         end
+       }
+       all
+  end
+end
