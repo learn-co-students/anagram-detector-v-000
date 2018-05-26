67e3c5b3c7f1906e9842459037dc4c9f00eae972
Taylor Williams <taylor@thewilliams.net>
2018-05-26 04:30:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |wordsa|  wordsa.chars.to_a.sort == word.chars.to_a.sort }
+  end
+end
