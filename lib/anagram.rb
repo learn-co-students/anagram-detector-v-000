a700d48264ed13c2bd29163311b638fd8258672c
Eric Chrobak <eric@ericchrobak.com>
2018-05-31 01:55:44 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(matches)
+    new_word = ""
+    old_word = @word.split("").sort
+    new_matches = []
+    matches.each do |word|
+      new_word = word.split("").sort
+      if old_word == new_word
+        new_matches << word
+      end
+    end
+    new_matches
+  end
+end
