305284245795c7bf35bc808fddfc9e8042318193
Janice Darikho <janicedarikho@gmail.com>
2018-05-31 14:13:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :anagram
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  
+  def match(words)
+    split_word = @anagram.split("").sort
+    arr = []
+    words.map { |word|
+      if word.split("").sort == split_word
+        arr << word
+      end
+    }
+    arr
+  end
+end
