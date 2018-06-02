63c9358ddd3a0aff81feb49588c6e1a1402cceea
Elaine Anderson <eparie@bu.edu>
2018-06-02 15:00:39 +0000

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
+require 'pry'
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(words_array)
+  words_array.flat_map do |w|
+    sorted_array = w.split("")
+    sorted_word = @word.split("")
+  #  sorted_array.collect do |x|
+ if sorted_array.sort == sorted_word.sort
+      array = sorted_array.join
+    #  array.delete_if{|elem| elem.empty?}
+
+
+    else
+    []
+  end
+end
+end
+
+
+
+end
