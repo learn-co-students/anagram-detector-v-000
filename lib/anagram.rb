ced4075ab512f08bf45bf4039cee7064362d878f
Richard Fenix <richard.fenix@gmail.com>
2018-06-11 15:04:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(list)
+    some_word = @word.split('').sort
+    list.map {|x|
+    x if x.split('').sort == some_word
+    }.compact
+  end
+
+end
+
+# test by running ruby lib/anagram.rb
+# listen = Anagram.new("listen")
+# puts listen.match(["enlists", "google", "inlets", "banana"]).inspect
