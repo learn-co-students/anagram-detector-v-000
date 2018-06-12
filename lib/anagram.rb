f6317e07ce8e58ed2c509a6edd94c5f40f880a33
Alicia Yu <alicia.yu.16@gmail.com>
2018-06-12 23:21:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arrgument)
+    arrgument.select do |string|
+      word.split('').sort == string.split('').sort
+    end
+  end
+end
