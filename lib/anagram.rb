3606dd59554fe33a95d0eb45c497c858d291315d
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-21 16:30:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,15 @@
 class Anagram
 
-  attr_accessor :word_to_analyze
+  attr_accessor :word
   
-  def initialize(word_to_analyze)
-    @word_to_analyze = word_to_analyze
+  def initialize(word)
+    @word = word
   end
 
   def match(array_of_words)
     array_of_words.select do | word |
-      (@word.split("").sort) == (word_to_analyze.split("").sort)
+      word.split("").sort == @word.split("").sort
     end
   end
+
 end
