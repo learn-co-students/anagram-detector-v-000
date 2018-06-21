5e95e50e0567a38cc1d28b6a3458fd44498c9096
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-21 16:24:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,9 @@ class Anagram
     @word_to_analyze = word_to_analyze
   end
 
-  def match(find_an_anagram_in_this_string)
-    puts "Looking to see if #{self.word_to_analyze} is in the string: #{find_an_anagram_in_this_string}"
-    find_an_anagram_in_this_string.find_all do | word |
-      if word.split("").sort == self.word_to_analyze("").sort
-      word
-      end
+  def match(array_of_words)
+    array_of_words.select do | word |
+      (@word.split("").sort) == (word_to_analyze.split("").sort)
     end
   end
 end
