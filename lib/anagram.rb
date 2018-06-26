58d66419d5c19b76f58bf70960afe5c30897f22b
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-26 13:21:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    # binding.pry 
+    array.select do |the_word|
+      split_word = the_word.split("").sort
+      original_word = @word.split("").sort
+      # binding.pry
+      if split_word == original_word
+        the_word
+      end 
+    end 
+  end 
+    
+end 
