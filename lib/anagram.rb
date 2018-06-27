7ea7d01a4c2723f3311baccd88781073e3b04c6a
Amit Singh <amit_nyc@live.com>
2018-06-27 15:40:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,24 @@
 # Your code goes here!
+
+
+#create a class called Anagram
 class Anagram
 
+
+#build a reader & writer method using attr_accessor
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
+  initialize with a word
+
+  def initialize(word)
+    @word = word
   end
 
 
 def match(array_of_words)
 
   array_of_words.select do|element|
-    (@name.split("").sort) == (element.split("").sort)
+    (@word.split("").sort) == (element.split("").sort)
   end
 end
 
