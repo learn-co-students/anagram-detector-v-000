dc945f3a074cf9cb5b98367f2853037acbca42f5
Valerie Moore <periwinkle17@outlook.com>
2018-06-27 19:35:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    @word = @word.chars.sort.join
+    word_array.select {|word| word.chars.sort.join == @word}
+  end
+
+end
