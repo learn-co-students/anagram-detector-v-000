4d5ff4e009216514abf33e982dfa73c21e203bf4
Amit Singh <amit_nyc@live.com>
2018-06-27 15:34:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+def match(array_of_words)
+
+  array_of_words.select do|element|
+    (@name.split("").sort) == (element.split("").sort)
+  end
+end
+
+
+
+
+end
