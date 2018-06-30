63dc279e050cd01dee58f4aaca5ffe3ab3954878
Edsel Caprice <caprice@mail.usf.edu>
2018-06-30 18:29:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+@word = word
+end
+
+def match(array)
+  anagram = []
+ array.each do |words|
+   if words.chars.sort.join == @word.chars.sort.join
+     anagram << words
+   end
+ end
+ anagram
+end
+
+
+
+end
