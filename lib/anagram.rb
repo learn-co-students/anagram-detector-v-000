73cf0169a7cae24f1ac4043fc122de816bc14de2
Terri Riggle <codeofarmour@outlook.com>
2018-07-02 21:14:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(matches)
+    result = []
+    matches.each do |word|
+      result << word if word.split('').sort == @word.split('').sort
+    end
+    result
+  end
+end
