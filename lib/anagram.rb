b88f98b72ac7da5b57d8ba112d2cfea74de63982
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-03 20:39:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,18 +3,29 @@ class Anagram
 
 attr_accessor :word
 
-@@all = []
+#@@all = []
 
   def initialize(word)
     @word = word
   end
 
-  def self.all
-    @@all
-  end
+  #def self.all
+  #  @@all
+  #end
 
-  def match(word_array) 
-    @word = @word.chars.sort.join 
-    word_array.select {|word| word.chars.sort.join == @word} 
+  def match(word_array)
+
+    new_array = []
+    new_word = @word.split("")
+    word_array.each do |word|
+      if word.split("").sort == new_word.sort
+        new_array << word
+
+      end
+    end
+    new_array
+    # @word.chars.sort.join 
+
+    #word_array.select {|word| word.chars.sort.join == @word} 
   end
 end
