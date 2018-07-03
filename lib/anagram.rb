3d3b504a88abd64d32e048ba5981b014ae294fcc
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-03 19:45:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+@@all = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def self.all
+    @@all
+  end
+
+  def match(word_array) 
+    @word = @word.chars.sort.join 
+    word_array.select {|word| word.chars.sort.join == @word} 
+  end
+end
