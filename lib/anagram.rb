54c5fdb7a7b81a4d39701143fedfaaf2cf03e0c6
Will Schultz <will.j.schultz@gmail.com>
2018-07-11 04:15:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name, :original
+
+  def initialize(original)
+    @name = original
+
+  end #initialize
+
+  def match(inputs)
+    inputs.select do |x|
+      x.split("").sort == @name.split("").sort
+    end
+  end #match
+
+end #Anagram
