ec2cab3b7ccb1380e1683a5eaebda0349c2010a3
Jordan Harris <jharris@wework.com>
2018-07-12 21:00:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(word_arr)
+    word_arr.select {|i| i.split("").sort == @word.split("").sort}
+  end
+  
+end
