965d0d3a52ac761066a6168032239a997cdd7789
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-07-17 22:50:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(name_arr)
+    name_arr.select {|n| n.split("").sort == @name.split("").sort}
+  end
+end
