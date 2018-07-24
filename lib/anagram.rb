753bec1805bcc26049cb257495bedae8ee621725
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-24 11:10:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word)
+   word.select  {|w| @word.split("").sort == w.split("").sort}
+  end
+end
