297fccd973215dfce2519ad4838769de4eb93081
Anita Beauchamp <amadamspr@gmail.com>
2018-07-28 22:57:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    letters = @word.split(//)
+    anagrams.select do |anagram|
+      anagram_letters = anagram.split(//)
+      anagram if anagram_letters.sort == letters.sort
+    end
+  end
+end
