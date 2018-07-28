b9622af6500e38748b295b5a8d36ee38ce1eef0f
Christopher Beltran <christod.beltran@gmail.com>
2018-07-28 22:49:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :gram
+    
+def initialize(gram)
+  @gram = gram
+end
+
+def match(list)
+  matches = []
+  list.each do |word|
+  letters = word.scan /\w/
+  grams = @gram.scan /\w/
+  if letters.sort == grams.sort
+    matches << word
+end
+end 
+return matches
+end 
+end 
