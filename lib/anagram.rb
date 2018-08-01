ceba9d123ebc4e2141915b8431155de1e331926e
Amee Covarrubias <ameecova@yahoo.com>
2018-08-01 18:37:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :letters
+  
+  def initialize(string)
+    @letters = string
+  end 
+  
+  def match(arr)
+    arr.select do |word| 
+      if word.split("").sort == @letters.split("").sort
+        word
+      end
+    #binding.pry
+    end
+  end 
+end 
