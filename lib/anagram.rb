44dd6f0ce8353ad022baa79e7e91842c0d647a23
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-14 19:10:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_arr)
+    anagram_arr.collect{|gram| gram if gram.split("").sort == word.split("").sort}.compact!
+  end
+
+
+end
