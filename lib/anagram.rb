ee400b25e46fff51e1201d0240ed7dbcb9d7460d
Jonathan Chan <johnkchan252@gmail.com>
2018-08-16 03:09:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+  def initialize(keyword)
+    @keyword = keyword
+  end
+  
+  def match(array)
+    output = []
+    array.each do |word|
+      if @keyword.split("").sort == word.split("").sort
+        output << word
+      end
+    end
+    output
+  end
+  
+end
