06d20e677a06cd26709d516b9bf78c53e7b64e4c
Austin Powell <austinpowell58@gmail.com>
2018-08-18 16:56:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(stringofstuff)
+    final_answer = []
+    word_array = @word.split("").sort
+    stringofstuff.each do |x|
+      x_array = x.split("").sort
+      if x_array == word_array
+        final_answer << x
+      end 
+    end 
+   final_answer
+  end 
+  
+  
+end 
