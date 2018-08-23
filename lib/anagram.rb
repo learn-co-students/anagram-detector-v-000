02ca7a86ebf7d6ead9d692d674dd98604bad4758
Daniel Schoch <d.schoch90@gmail.com>
2018-08-23 18:19:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    list.select do |comparison|
+      comparison.split(//).sort == @word.split(//).sort
+    end
+  end
+end
