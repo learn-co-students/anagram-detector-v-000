bf116f39f7a5a306da783fded0e68be512dcc56d
Achsah Abraham <achsahmp@gmail.com>
2018-08-23 17:21:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :no_matches
+  def initialize(no_matches)
+    @no_matches = no_matches
+  end
+
+  def match(anagrams)
+    anagrams.select do |no_matches|
+      @no_matches.split("").sort == no_matches.split("").sort
+    end
+  end
+end
