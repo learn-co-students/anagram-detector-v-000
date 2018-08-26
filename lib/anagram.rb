bdab9c2de547e3498dcee1e4c63c8f8753e3d867
Heshie Brody <4hbrody@gmail.com>
2018-08-26 17:32:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+# 1. split words into character arrays
+# 2. sort each array
+# 3. compare arrays and return anagram
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  def match(possible_anagram)
+    matches = [ ]
+    possible_anagram.each do |w|
+      matches << w if w.split("").sort == @word.split("").sort
+    end
+    matches
+  end
+end
