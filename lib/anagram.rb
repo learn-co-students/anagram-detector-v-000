a61a752a4bcec34fa8bfa55999ad57f5fedb4ed9
Laura Darlak <laura@polyporedesign.com>
2018-08-27 16:35:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+  
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(anagram_a)
+  match_a = []
+  anagram_a.each do |anagram|
+    anagram = anagram.split("")
+    if anagram.sort == @word.split("").sort 
+      match_a << anagram.join
+    else match_a
+    end
+  end
+  match_a
+end
+
+end
