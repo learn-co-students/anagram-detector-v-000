c27932e9eafaf6072bb39f4ba51d1e828b4a40c3
Kenzie Morlock <k.morlock86@gmail.com>
2018-09-13 22:16:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :taco
+
+  def initialize(word)
+    @taco = word
+  end
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @taco.split("").sort
+    end
+  end
+end
