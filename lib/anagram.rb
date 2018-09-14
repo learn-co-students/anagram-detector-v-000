79d7fe495667d0d26681b774988f24e10c96d615
Nick Romans <nicholas.romans@gmail.com>
2018-09-14 19:10:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+  
+def match(array_of_anagrams)
+  array_of_anagrams.find_all do |word_to_match|
+    if @word.split("").sort == word_to_match.split("").sort
+  word_to_match
+  end
+end 
+end
+
+end 
