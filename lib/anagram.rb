edf84070f5d82ef1ae9dff7964c376198f8a8769
Ashley Wong <ashwong04@gmail.com>
2018-09-16 13:03:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,14 +12,12 @@ class Anagram
     sort_word = @word.split(//).sort
     anagram_list = []
     
-    list.each do |l|
-      list_word = l.split(//).sort
-      if  list_word == sort_word
-       anagram_list << l
+    list.collect do |l|
+      if l.split(//).sort == sort_word
+        anagram_list << l
       end
-       return anagram_list
     end
-  
+      anagram_list
   end
   
 end
