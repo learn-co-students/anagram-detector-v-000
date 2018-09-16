3f94fdc787744e1abf8180192d36828aa7ae8618
Ashley Wong <ashwong04@gmail.com>
2018-09-16 01:32:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(list)
+    sort_word = @word.split().sort
+    list.collect do |l|
+      if l.split().sort == sort_word
+       return l
+      else
+        return []
+      end
+    end
+  end
+  
+end
