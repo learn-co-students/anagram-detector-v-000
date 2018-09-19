d46ed88b7b19c89db62ef34f536dfc8e5df4e849
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-19 18:13:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,6 @@ class Anagram
   end
 
   def match(words)
-    word_letters = word.split("")
-    letters = words.map { |w| w.split("") }
-    matches = letters.find_all { |letter_array| letter_array.sort == word_letters.sort }
-    matches.map { |w| w.join }
+    words.find_all { |w| w.split("").sort == word.split("").sort }
   end
 end
