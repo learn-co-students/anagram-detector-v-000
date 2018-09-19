5430d5b7053c5283f8367c415f9550e1dd7ba816
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-19 18:09:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    word_letters = word.split("")
+    letters = words.map { |w| w.split("") }
+    matches = letters.find_all { |letter_array| letter_array.sort == word_letters.sort }
+    matches.map { |w| w.join }
+  end
+end
