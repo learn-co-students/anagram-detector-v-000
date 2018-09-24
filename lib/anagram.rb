f5a859059324e419418cb0d0fb233241d831df1c
Sherry Martin <sm674@cornell.edu>
2018-09-24 01:05:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(array)
+    array.keep_if { |str| name.split('').sort == str.split('').sort }
+  end
+end
