c7d3bd7cc61cb51e7f9b0401cfd925557598211e
Cheryl K <cherylkelson@icloud.com>
2018-09-29 21:42:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  @@all = []
+ 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+ 
+  def match(array)
+    array.select {|x| x.split("").sort == x.split("").sort 
+    binding.pry
+    
+  end
+  
+end
+  
+ 
+
