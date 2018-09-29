90dde0f9d4d116d63b09c00465bb497930a3a993
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-29 14:58:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word_to_find
+
+  def initialize(word_to_find)
+    @word_to_find = word_to_find 
+  end 
+  
+  def match(str_with_anagram)
+    new_arr = []
+    str_with_anagram.find_all do |word| 
+      if word.split("").sort == self.word_to_find.split("").sort 
+        new_arr << word
+      end
+    end 
+      return new_arr
+  end
+end 
