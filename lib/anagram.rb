e8511e03999d7ac9cf31db3a679c4da40c92a6fd
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-10-04 04:39:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(possible_anagrams)
+    
+    matches = []
+    
+    possible_anagrams.each do |words|
+      
+      array1 = @word.split("").sort 
+      array2 = words.split("").sort
+       #binding.pry
+      if array1 == array2
+        matches << words
+        matches
+      end
+    end
+    
+    matches
+  end
+end
