9461c889f3bc0eeb8391db8f45bae5f5c7a4d3fd
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-04 08:52:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word_being_matched_against, :comparison_control
+  
+  def initialize(string)
+    @word_being_matched_against = string 
+    @comparison_control = string.split("").sort
+  end 
+
+  
+  def match(array_of_words)
+     matches = []
+     array_of_words.each do |word|
+       comparison_check = word.split("").sort 
+       
+       matches << word if comparison_check == @comparison_control
+       
+     end 
+     matches 
+  end 
+  
+end 
