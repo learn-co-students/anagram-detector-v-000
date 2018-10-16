dcfa861335541b0598b857fb61e063283d5c6a8e
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-16 20:38:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,28 +1,14 @@
-require 'pry'
 class Anagram
   attr_accessor :word
   
   def initialize(word)
     @word = word
   end
-  
-  
 
   def match(possible_anagrams)
     possible_anagrams.delete_if do |possible|
-      word.split("").sort == !possible.split("").sort
+      word.split("").sort != possible.split("").sort
     end
     possible_anagrams
-    binding.pry
   end
 end
-
-def match(possible_anagrams)
-    anagrams = []
-    possible_anagrams.each do |possible|
-      if word.split("").sort == possible.split("").sort
-        anagrams << possible
-        #binding.pry
-      end
-    end
-  end
