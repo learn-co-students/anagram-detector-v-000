6d4c9d65c7a3055d64bee099cfe43ccd82521831
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 03:39:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram)
+    # anagram.map do |word|
+    #   @word.split("").sort == word.split("").sort ? word : nil
+    # end.compact
+    anagram.find_all { |word| @word.split("").sort == word.split("").sort }
+  end
+end
