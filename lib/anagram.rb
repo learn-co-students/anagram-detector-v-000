6576c0368f7ed3f6bce3405206802f35e7398155
Niusha H <niusha.hadziomerovic@gmail.com>
2018-11-07 19:28:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+attr_accessor :does_this_word_have_an_anagram_here
+
+#Yes the word initialized is the word that is being tested against. 
+
+def initialize(does_this_word_have_an_anagram_here)
+  @does_this_word_have_an_anagram_here = self.new 
+end
+
+#WRITE A LOOP --> break down all letters in the word and see if they exactly match the letters of a different word(an anagram)
+
+look_through_list_of_anagrams.match all letters in %w(does_this_word_have_an_anagram_here)
+
+
+
+#WRITE CONDITIONAL CODE --> return an array of all matching words, if there are any, OR if no match, return empty array
+
+
+
+end
