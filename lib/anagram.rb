3ae442308bf4711eeb448207851d48984f64707f
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-08 02:51:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    word = @word.split(//).sort
+    words.select do |w|
+      w.split(//).sort == word
+    end
+  end
+end
