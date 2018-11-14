695d8df2e2f2c407bfd45935ef3b0619814c9dee
Lois Lee <lois.lee@me.com>
2018-11-14 18:08:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.find_all do |thing|
+      thing.split("").sort == word.split("").sort
+    end
+  end
+
+end
