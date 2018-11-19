51605c1f254065036ef8f480dec57bfe34f0826c
Brad Schrag <brad.schrag@gmail.com>
2018-11-19 18:18:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list_of_words)
+    self_dictionary = word_to_dictionary(@word)
+    match_list = []
+    list_of_words.each do | word |
+      if word_to_dictionary(word) == self_dictionary
+        match_list << word
+      end
+    end
+    match_list
+  end
+
+  def word_to_dictionary(word)
+    dictionary = {}
+    for letter in word.split('')
+      if dictionary.include?(letter)
+        dictionary[letter] += 1
+      else
+        dictionary[letter] = 1
+      end
+    end
+    dictionary
+  end
+
+end
