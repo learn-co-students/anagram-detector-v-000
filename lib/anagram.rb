8373ee5836f8783f1a5e7523456804aa776f561f
Niusha H <niusha.hadziomerovic@gmail.com>
2019-01-07 07:05:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,20 +2,36 @@
 class Anagram
 attr_accessor :does_this_word_have_an_anagram_here
 
+
+
 #Yes the word initialized is the word that is being tested against. 
 
-def initialize(does_this_word_have_an_anagram_here)
-  @does_this_word_have_an_anagram_here = self.new 
-end
-
-#WRITE A LOOP --> break down all letters in the word and see if they exactly match the letters of a different word(an anagram)
-
-look_through_list_of_anagrams.match all letters in %w(does_this_word_have_an_anagram_here)
+  def initialize(does_this_word_have_an_anagram_here)
+    @does_this_word_have_an_anagram_here = self.new 
+  end
 
 
-
-#WRITE CONDITIONAL CODE --> return an array of all matching words, if there are any, OR if no match, return empty array
-
-
+  
+    #WRITE A LOOP --> break down all letters in the word and see if they exactly match the letters of a different word(an anagram)
+    
+    #check if both words have same number of letters
+    
+    if word.count = anagram_instance.count
+      
+    #split the words into array of letters  
+      array_of_words_letters = word.split("")
+      array_of_instance_letters = instance.split("")
+      
+    #check to see if words match  
+        if array_of_words_letters.match(%w(possible anagram words)) == array_of_instance_letters.sort
+          return instance
+        else 
+          return empty_array
+        end  
+    end
+    
+    #WRITE CONDITIONAL CODE --> return an array of all matching words, if there are any, OR if no match, return empty array
+  
+  end
 
 end
