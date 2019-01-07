92880bd844a226099e894721ae3c2280e51b0528
Niusha H <niusha.hadziomerovic@gmail.com>
2019-01-07 07:39:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,13 +3,11 @@ class Anagram
 attr_accessor :does_this_word_have_an_anagram_here
 
 
-
 #Yes the word initialized is the word that is being tested against. 
 
   def initialize(does_this_word_have_an_anagram_here)
     @does_this_word_have_an_anagram_here = self.new 
   end
-
 
   
     #WRITE A LOOP --> break down all letters in the word and see if they exactly match the letters of a different word(an anagram)
@@ -17,17 +15,17 @@ attr_accessor :does_this_word_have_an_anagram_here
     #check if both words have same number of letters
     
     if word.count = anagram_instance.count
+      @does_this_word_have_an_anagram_here.each do |does_this_word_have_an_anagram_here|
       
-    #split the words into array of letters  
-      array_of_words_letters = word.split("")
-      array_of_instance_letters = instance.split("")
+        #split the words into array of letters  
+        array_of_words_letters = word.split("").sort
+        array_of_instance_letters = instance.split("").sort
       
-    #check to see if words match  
-        if array_of_words_letters.match(%w(possible anagram words)) == array_of_instance_letters.sort
-          return instance
-        else 
-          return empty_array
-        end  
+      #check to see if words match  
+      if array_of_words_letters.match(%w(possible anagram words)) == array_of_instance_letters.sort
+      return array of all matching words
+    else 
+      return empty_array
     end
     
     #WRITE CONDITIONAL CODE --> return an array of all matching words, if there are any, OR if no match, return empty array
