f62ab0ea4a7f30b250316c9a818d2c71a9491931
Karly Mareka <karly.mareka@gmail.com>
2019-01-13 18:19:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    matches = []
+    array.each do |possible_match|
+      match_letters = possible_match.split(//)
+      word_letters = word.split(//)
+        if match_letters.sort == word_letters.sort
+          matches << possible_match
+        end 
+    end
+    matches 
+  end 
+  
+end 
