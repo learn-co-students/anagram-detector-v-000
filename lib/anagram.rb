afdf62a6979977b8cedcfeb165e0a59883ec04e9
Ron Lynch <lynch.ronald1@gmail.com>
2019-01-16 02:18:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+  class Anagram
+    attr_accessor :gram
+
+    def initialize(gram)
+      @gram = gram
+    end
+
+    def match(gram)
+      gram.select {|puzzle| puzzle.split("").sort == @gram.split("").sort}
+    end
+  end
+
+  #Write method that DETECTS no matches within an array
+  #Write method that can DETECT a simple anagram
+  #Write method that can DETECT a regular anagram
+  #Write method that can DETECT all anagrams
