3da685a263a577c2e62dbed35b8cba73871250c4
Brandon Brasson <rocksong622@gmail.com>
2019-02-07 15:48:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+
+[1, 3, 2].sort == [3, 2, 1].sort
+
+
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word)
+#word.each do
+ #return []
+  word.select do |word|
+  word.split("").sort == @word.split("").sort
+end
+end
+
+
+
+end
