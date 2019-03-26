302e03e9cdeec18a170dd9c4f0cbe0075d83a539
Carden Z <ephemere@protonmail.com>
2019-03-26 21:00:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+	attr_reader :word
+
+  def initialize(word)
+  	@word = word
+  end
+
+  def match(arr)
+    arr.select do |w|
+	    w.split('').sort == self.word.split('').sort
+	  end
+  end
+end
