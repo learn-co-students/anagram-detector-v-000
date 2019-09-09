84756ad8e4b2e634b1f1215cd9cbb6b20d73bee8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-09-09 21:41:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  #def initialize(word)
+    #@word = word
+    #match(%w())
+    #match()
+  #end
+
+  #def match(match)
+  #def match()
+    #%w()
+  #end
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+
+    @matches = list.select { |item| item.split("").sort == @word.split("").sort }
+
+    if !@matches.empty?
+      @matches
+    else
+      %w()
+    end
+  end
+
+end
