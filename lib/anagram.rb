e7fb2705240655b00b9cbd6d69053ab878d7bc3a
Eva Navarro <emanao@me.com>
2019-11-11 20:13:17 +0000

    Optimize match method using select enumerator

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,8 @@ class Anagram
     @word=word
   end
   def match (anagram_array)
-    result= []
-    word_chars = word.split(//).sort
-    anagram_array.each do |anagram|
-      anagram_chars = anagram.split(//).sort
-      result <<anagram if word_chars == anagram_chars
-    end
-    result
+    anagram_array.select {|anagram|
+      anagram.strip.split(//).sort == word.strip.split(//).sort
+    }
   end
 end
