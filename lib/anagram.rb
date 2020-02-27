99483153ce796c31ae70b1f8e45a9318c7e34eae
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-02-27 02:01:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    
+    w = word.split("").sort
+    
+    # array_sort = array.map{|w| w.split("").sort}.reject {|e| e!=w} 
+    
+    array.select{|e| e.split("").sort == w } #--> 
+
+  end
+  
+end
