38c0dc0c41f73b9a8dc308cddf6d331f018943c5
hkato4188 <hkato@sas.upenn.edu>
2020-07-20 16:35:14 -0400

    refactored Anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,37 +2,14 @@
 require 'pry'
 
 class Anagram
-  attr_accessor :word
 
-  def initialize(string)
-    @word = string
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
   end
 
-  def match(possible_anagrams)
-    possible_anagrams.select do |possible_anagram|
-      possible_anagram.split("").sort == @word.split("").sort
-    end
+  def match(list)
+    list.select {|word| word.split("").sort == @anagram.split("").sort}
   end
 end
-
-
-
-# class Anagram
-#   attr_reader :word
-#
-#   def initialize(word)
-#     @word = word
-#   end
-#
-#   def match(possible_anagrams)
-#     anagram_match = []
-#     possible_anagrams.each do |possible_anagram|
-#       if possible_anagram.split("").sort == word.split("").sort
-#         anagram_match << possible_anagram
-#       else
-#         nil
-#       end
-#     end
-#     anagram_match
-#   end
-# end
