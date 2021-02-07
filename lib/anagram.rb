e355543341f46fcb717ceae0600cf48759e9d509
Christopher Garcia <christopher.garcia08@gmail.com>
2021-02-07 00:10:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word, :nagaram
+  def initialize(nagaram)
+    @nagaram = nagaram
+  end
+ 
+  def check(fora)
+    fora.chars.sort == @nagaram.chars.sort
+  end
+  
+  def match(word)
+    word.select do |word|
+      check(word)
+    end
+  end
+      
+end
